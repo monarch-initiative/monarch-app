19d71d8b2a6570bce2de5be61424793a23a4fb81
caufieldjh <j.harry.caufield@gmail.com>
2022-11-15 13:53:59 -0800

    Linting; process string args to lists

--- backend/monarch_api/semsim.py
+++ backend/monarch_api/semsim.py
@@ -1,15 +1,12 @@
 from fastapi import APIRouter, Depends
 from monarch_api.additional_models import PaginationParams
-
 from monarch_api.utils.get_similarity import *
+from oaklib.cli import _shorthand_to_pred_curie
 
 router = APIRouter(
-    prefix="/api/semsim",
-    tags=["semsim"],
-    responses = {
-        404: {"description":"Not Found"}
-    }
+    prefix="/api/semsim", tags=["semsim"], responses={404: {"description": "Not Found"}}
 )
+
 
 @router.get("/semsim/{subjlist}/{objlist}")
 async def _get_termlist_similarity(
@@ -19,7 +16,11 @@ async def _get_termlist_similarity(
     predicates: str = "",
 ):
 
-    # TODO: process string values to lists
+    # Process string values to lists
+    for list_type in [subjlist, objlist, predicates]:
+        if "," in list_type:
+            list_type = list_type.split(",")
+    predicates = [_shorthand_to_pred_curie(p) for p in predicates]
 
     results = termlist_similarity(
         subjlist=subjlist,
