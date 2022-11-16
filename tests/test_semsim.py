5d6bd3bdf03b10674fe85281a29ff859ab5213e4
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-11-15 17:01:22 -0800

    fixing test

--- tests/test_semsim.py
+++ tests/test_semsim.py
@@ -1,8 +1,6 @@
-import pytest
 from oaklib import OntologyResource
 from oaklib.implementations.sqldb.sql_implementation import SqlImplementation
-from pprint import pprint
-HP_DB_URL = "https://s3.amazonaws.com/bbop-sqlite/hp.db.gz"
+HP_DB_URL = "phenio.db"
 
 resource = OntologyResource(slug=HP_DB_URL)
 oi = SqlImplementation(resource)
@@ -10,6 +8,7 @@ oi = SqlImplementation(resource)
 
 def test_oak():
     print(oi.label("HP:0000498"))
+
     sim = oi.termset_pairwise_similarity(["HP:0001036"],
                                          ["HP:0002583"])
     assert sim.subject_best_matches["HP:0001036"]
