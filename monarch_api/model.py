0925ed7bdd1341c0fabbf7267aeb6b9690107eb4
caufieldjh <j.harry.caufield@gmail.com>
2022-11-15 17:05:42 -0800

    Lint lint

--- monarch_api/model.py
+++ monarch_api/model.py
@@ -7,20 +7,24 @@ from pydantic import BaseModel as BaseModel, Field
 metamodel_version = "None"
 version = "None"
 
+
 class WeakRefShimBaseModel(BaseModel):
-   __slots__ = '__weakref__'
-    
-class ConfiguredBaseModel(WeakRefShimBaseModel,
-                validate_assignment = True, 
-                validate_all = True, 
-                underscore_attrs_are_private = True, 
-                extra = 'forbid', 
-                arbitrary_types_allowed = True):
-    pass                    
+    __slots__ = "__weakref__"
+
+
+class ConfiguredBaseModel(
+    WeakRefShimBaseModel,
+    validate_assignment=True,
+    validate_all=True,
+    underscore_attrs_are_private=True,
+    extra="forbid",
+    arbitrary_types_allowed=True,
+):
+    pass
 
 
 class Node(ConfiguredBaseModel):
-    
+
     id: str = Field(None)
     label: Optional[str] = Field(None)
     iri: Optional[str] = Field(None)
@@ -34,39 +38,33 @@ class Node(ConfiguredBaseModel):
     taxon: Optional[Taxon] = Field(None)
     inheritance: Optional[Inheritance] = Field(None)
     association_counts: Optional[List[AssociationCount]] = Field(default_factory=list)
-    
 
 
 class Inheritance(ConfiguredBaseModel):
-    
+
     id: Optional[str] = Field(None)
     pred: Optional[str] = Field(None)
     xrefs: Optional[str] = Field(None)
-    
 
 
 class Synonym(ConfiguredBaseModel):
-    
+
     id: Optional[str] = Field(None)
     label: Optional[str] = Field(None)
     iri: Optional[str] = Field(None)
-    
 
 
 class Taxon(ConfiguredBaseModel):
-    
+
     id: Optional[str] = Field(None)
     label: Optional[str] = Field(None)
-    
 
 
 class AssociationCount(ConfiguredBaseModel):
-    
+
     id: Optional[str] = Field(None)
     counts: Optional[str] = Field(None)
     counts_by_taxon: Optional[str] = Field(None)
-    
-
 
 
 # Update forward refs
@@ -76,4 +74,3 @@ Inheritance.update_forward_refs()
 Synonym.update_forward_refs()
 Taxon.update_forward_refs()
 AssociationCount.update_forward_refs()
-
