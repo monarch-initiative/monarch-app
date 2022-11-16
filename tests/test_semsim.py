df7920801a7eb189ef01309dbfd3fde05d894252
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-11-16 14:20:46 -0800

    add prints to tests

--- tests/test_semsim.py
+++ tests/test_semsim.py
@@ -4,19 +4,20 @@ from oaklib.implementations.sqldb.sql_implementation import SqlImplementation
 from unittest import TestCase
 
 DB_PATH = "sqlite:obo:bfo"
+# DB_PATH = "sqlite:///tests/data/go-nucleus.db" # move to go-nucleus, add tests/input dir
 
 
 class TestSemsim(TestCase):
 
     def setUp(self) -> None:
         self.oi = GrapeImplementation(OntologyResource(slug=DB_PATH))
-        self.test_node = "BFO:0000001"
+        self.test_node = "BFO:0000006"
 
     def test_semsim(self):
         label = self.oi.label(self.test_node)
 
         sim = self.oi.termset_pairwise_similarity([self.test_node],
-                                                  ["BFO:0000002"])
+                                                  ["BFO:0000015"])
         
         score = sim.subject_best_matches[self.test_node].score
 
