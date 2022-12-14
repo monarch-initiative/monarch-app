8ff5202baee8f0197d1a24eb249bfc8b7a6227ed
Kevin Schaper <kevinschaper@gmail.com>
2022-12-14 15:20:46 -0800

    Added node hierarchy support for the node response

--- src/monarch_api/utils/entity_utils.py
+++ src/monarch_api/utils/entity_utils.py
@@ -1,12 +1,19 @@
-from monarch_api.model import Association, Entity, NodeHierarchy
+from typing import List
+
 from monarch_py.implementations.solr.solr_implementation import SolrImplementation
+
+from monarch_api.model import Association, Entity, NodeHierarchy
 
 
 def get_associated_entity(association: Association, this_entity: Entity) -> Entity:
     """
     Convert an Association to an Entity by extracting the subject or object
     (whichever is not this_entity) and setting the id, name and category on
-    the returned Entity
+    the returned Entity.
+
+    If ever we need to add more fields, we'll need to add additional expansions
+    in the closurizer repo that produces the denormalized kgx file that we use
+    to populate the solr index.
 
     Args:
         association (Association): A single association, expected to contain `this_entity`
@@ -16,19 +23,52 @@ def get_associated_entity(association: Association, this_entity: Entity) -> Enti
         Entity: A limited representation of the entity associated with `this_entity`
     """
     if association.subject == this_entity.id:
-        entity = Entity(id = association.object,
-                        name = association.object_label,
-                        category = association.object_category)
+        entity = Entity(
+            id=association.object,
+            name=association.object_label,
+            category=association.object_category,
+        )
     elif association.object == this_entity.id:
-        entity = Entity(id = association.subject,
-                        name = association.subject_label,
-                        category = association.subject_category)
+        entity = Entity(
+            id=association.subject,
+            name=association.subject_label,
+            category=association.subject_category,
+        )
     else:
         raise ValueError(f"Association does not contain this_entity: {this_entity.id}")
 
     return entity
 
-def get_hierarchy(entity: Entity, si: SolrImplementation) -> NodeHierarchy:
+
+def get_associated_entities(
+    this_entity: Entity,
+    si: SolrImplementation,
+    entity: str = None,
+    subject: str = None,
+    predicate: str = None,
+    object: str = None,
+) -> List[Entity]:
+    """
+    Get a list of entities associated with this_entity fetched from associations
+    in the Solr index
+
+    Args:
+        this_entity (Entity): The entity to get associations for
+        si (SolrInterface): A SolrInterface instance
+        entity (str, optional): an entity ID occurring in either the subject or predicate. Defaults to None.
+        subject (str, optional): an entity ID occurring in the subject. Defaults to None.
+        predicate (str, optional): a predicate value. Defaults to None.
+        object (str, optional): an entity ID occurring in the object. Defaults to None.
+    """
+    return [
+        get_associated_entity(association, this_entity)
+        for association in si.get_associations(
+            entity=entity, subject=subject, predicate=predicate, object=object, offset=0
+        ).associations
+    ]
+
+
+def get_node_hierarchy(entity: Entity, si: SolrImplementation) -> NodeHierarchy:
     """
     Get a NodeHierarchy for the given entity
 
@@ -39,10 +79,19 @@ def get_hierarchy(entity: Entity, si: SolrImplementation) -> NodeHierarchy:
     Returns:
         NodeHierarchy: A NodeHierarchy object
     """
-    # todo: review predicates, plan for empty results
-    return NodeHierarchy(
-        super_classes=si.get_associations(subject=entity.id, predicate="biolink:subclass_of").associations,
-        equivalent_classes=si.get_associations(entity=entity.id, predicate="biolink:same_as").associations,
-        sub_classes=si.get_associations(object=entity.id, predicate="biolink:subclass_of").associations,
+
+    super_classes = get_associated_entities(
+        entity, si, subject=entity.id, predicate="biolink:subclass_of"
+    )
+    equivalent_classes = get_associated_entities(
+        entity, si, entity=entity.id, predicate="biolink:same_as"
+    )
+    sub_classes = get_associated_entities(
+        entity, si, object=entity.id, predicate="biolink:subclass_of"
     )
 
+    return NodeHierarchy(
+        super_classes=super_classes,
+        equivalent_classes=equivalent_classes,
+        sub_classes=sub_classes,
+    )
