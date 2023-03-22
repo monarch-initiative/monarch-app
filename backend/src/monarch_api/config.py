664275f958a50a782f4f30dba2d1afaa735f6446
Kevin Schaper <kevinschaper@gmail.com>
2023-03-22 09:09:01 -0700

    add the option to set SOLR_HOST & SOLR_PORT separately from solr url

--- backend/src/monarch_api/config.py
+++ backend/src/monarch_api/config.py
@@ -4,8 +4,11 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    solr_host = os.getenv("SOLR_HOST") if os.getenv("SOLR_HOST") else "127.0.0.1"
+    solr_port = os.getenv("SOLR_PORT") if os.getenv("SOLR_PORT") else 8983
+
     solr_url = (
-        os.getenv("SOLR_URL") if os.getenv("SOLR_URL") else "http://127.0.0.1:8983/solr"
+        os.getenv("SOLR_URL") if os.getenv("SOLR_URL") else f"http://{solr_host}:{solr_port}/solr"
     )
 
 
