14e30fc1cbdecab5e187ca16e3690e5cdedf7876
Kevin Schaper <kevinschaper@gmail.com>
2023-05-05 09:55:01 -0700

    Add association counts call to entity page

--- backend/src/monarch_api/entity.py
+++ backend/src/monarch_api/entity.py
@@ -13,7 +13,6 @@ router = APIRouter(
 @router.get("/{id}")
 async def _get_entity(
     id,
-    get_association_counts: bool = False,
 ) -> Node:
     """Retrieves the entity with the specified id
 
@@ -50,13 +49,6 @@ async def _get_entity(
             )
 
     node.node_hierarchy = get_node_hierarchy(node, solr)
-
-    # todo: move association_counts query to it's own separate request
-    # need a monarch-py facet api
-    # for label in result["association_counts"]:
-    #     association_count = AssociationCount(
-    #         id=label, counts=result["association_counts"][label]
-    #     )
-    #     node.association_counts.append(association_count)
+    node.association_counts = solr.get_association_counts(id)
 
     return node
