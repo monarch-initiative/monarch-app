9f233804b1b46f8bd903baa3e2b3816af1d4d2bf
Kevin Schaper <kevinschaper@gmail.com>
2023-05-23 17:15:46 -0700

    Update to linkml 1.5.3 to get pydanticgen & typescriptgen improvements

--- backend/src/monarch_api/model.py
+++ backend/src/monarch_api/model.py
@@ -1,10 +1,17 @@
 from __future__ import annotations
 
+import sys
 from enum import Enum
 from typing import Dict, List, Optional
 
 from pydantic import BaseModel as BaseModel
 from pydantic import Field
+
+if sys.version_info >= (3, 8):
+    pass
+else:
+    pass
+
 
 metamodel_version = "None"
 version = "None"
@@ -27,29 +34,47 @@ class ConfiguredBaseModel(
 
 
 class AssociationDirectionEnum(str, Enum):
+    """
+    The directionality of an association as it relates to a specified entity, with edges being categorized as incoming or outgoing
+    """
 
+    # An association for which a specified entity is the object or part of the object closure
     incoming = "incoming"
+    # An association for which a specified entity is the subject or part of the subject closure
     outgoing = "outgoing"
 
 
 class AssociationTypeEnum(str, Enum):
+    """
+    A grouping label for association types, which are not necessarily 1:1 with biolink categories or predicates
+    """
 
+    # Any association between a disease and a phenotype
     disease_phenotype = "disease_phenotype"
+    # Any association between a gene and a phenotype
     gene_phenotype = "gene_phenotype"
+    # Any association between two genes
     gene_interaction = "gene_interaction"
+    # Any association between a gene and a pathway
     gene_pathway = "gene_pathway"
+    # Expression association between a gene and an expression site
     gene_expression = "gene_expression"
+    # Any association between two genes based on orthology
     gene_orthology = "gene_orthology"
+    # Any association between a chemical and a pathway
     chemical_pathway = "chemical_pathway"
+    # Any association between a gene and molecular activity
     gene_function = "gene_function"
+    # Association between a gene and a disease that has not been established to be causal
     correlated_gene = "correlated_gene"
+    # Association between a gene and a disease that is known to be causal
     causal_gene = "causal_gene"
 
 
 class Taxon(ConfiguredBaseModel):
 
-    id: str = Field(None)
-    label: str = Field(None)
+    id: str = Field(...)
+    label: str = Field(...)
 
 
 class NodeHierarchy(ConfiguredBaseModel):
@@ -62,16 +87,16 @@ class NodeHierarchy(ConfiguredBaseModel):
 class Association(ConfiguredBaseModel):
 
     aggregator_knowledge_source: Optional[List[str]] = Field(default_factory=list)
-    id: str = Field(None)
-    subject: str = Field(None)
+    id: str = Field(...)
+    subject: str = Field(...)
     original_subject: Optional[str] = Field(None)
     subject_namespace: Optional[str] = Field(None)
     subject_category: Optional[List[str]] = Field(default_factory=list)
     subject_closure: Optional[List[str]] = Field(default_factory=list)
     subject_label: Optional[str] = Field(None)
     subject_closure_label: Optional[List[str]] = Field(default_factory=list)
-    predicate: str = Field(None)
-    object: str = Field(None)
+    predicate: str = Field(...)
+    object: str = Field(...)
     original_object: Optional[str] = Field(None)
     object_namespace: Optional[str] = Field(None)
     object_category: Optional[List[str]] = Field(default_factory=list)
@@ -100,20 +125,20 @@ class DirectionalAssociation(Association):
     """
 
     direction: AssociationDirectionEnum = Field(
-        None,
+        ...,
         description="""The directionality of the association relative to a given entity for an association_count. If the entity is the subject or in the subject closure, the direction is forwards, if it is the object or in the object closure, the direction is backwards.""",
     )
     aggregator_knowledge_source: Optional[List[str]] = Field(default_factory=list)
-    id: str = Field(None)
-    subject: str = Field(None)
+    id: str = Field(...)
+    subject: str = Field(...)
     original_subject: Optional[str] = Field(None)
     subject_namespace: Optional[str] = Field(None)
     subject_category: Optional[List[str]] = Field(default_factory=list)
     subject_closure: Optional[List[str]] = Field(default_factory=list)
     subject_label: Optional[str] = Field(None)
     subject_closure_label: Optional[List[str]] = Field(default_factory=list)
-    predicate: str = Field(None)
-    object: str = Field(None)
+    predicate: str = Field(...)
+    object: str = Field(...)
     original_object: Optional[str] = Field(None)
     object_namespace: Optional[str] = Field(None)
     object_category: Optional[List[str]] = Field(default_factory=list)
@@ -138,7 +163,7 @@ class DirectionalAssociation(Association):
 
 class Entity(ConfiguredBaseModel):
 
-    id: str = Field(None)
+    id: str = Field(...)
     category: Optional[List[str]] = Field(default_factory=list)
     name: Optional[str] = Field(None)
     description: Optional[str] = Field(None)
@@ -157,7 +182,7 @@ class Node(Entity):
     inheritance: Optional[Entity] = Field(None)
     association_counts: Optional[List[AssociationCount]] = Field(default_factory=list)
     node_hierarchy: Optional[NodeHierarchy] = Field(None)
-    id: str = Field(None)
+    id: str = Field(...)
     category: Optional[List[str]] = Field(default_factory=list)
     name: Optional[str] = Field(None)
     description: Optional[str] = Field(None)
@@ -172,7 +197,7 @@ class Node(Entity):
 
 class HistoPheno(ConfiguredBaseModel):
 
-    id: str = Field(None)
+    id: str = Field(...)
     items: List[HistoBin] = Field(
         default_factory=list,
         description="""A collection of items, with the type to be overriden by slot_usage""",
@@ -181,9 +206,9 @@ class HistoPheno(ConfiguredBaseModel):
 
 class Results(ConfiguredBaseModel):
 
-    limit: int = Field(None, description="""number of items to return in a response""")
-    offset: int = Field(None, description="""offset into the total number of items""")
-    total: int = Field(None, description="""total number of items matching a query""")
+    limit: int = Field(..., description="""number of items to return in a response""")
+    offset: int = Field(..., description="""offset into the total number of items""")
+    total: int = Field(..., description="""total number of items matching a query""")
 
 
 class AssociationResults(Results):
@@ -192,9 +217,9 @@ class AssociationResults(Results):
         default_factory=list,
         description="""A collection of items, with the type to be overriden by slot_usage""",
     )
-    limit: int = Field(None, description="""number of items to return in a response""")
-    offset: int = Field(None, description="""offset into the total number of items""")
-    total: int = Field(None, description="""total number of items matching a query""")
+    limit: int = Field(..., description="""number of items to return in a response""")
+    offset: int = Field(..., description="""offset into the total number of items""")
+    total: int = Field(..., description="""total number of items matching a query""")
 
 
 class AssociationTableResults(Results):
@@ -203,9 +228,9 @@ class AssociationTableResults(Results):
         default_factory=list,
         description="""A collection of items, with the type to be overriden by slot_usage""",
     )
-    limit: int = Field(None, description="""number of items to return in a response""")
-    offset: int = Field(None, description="""offset into the total number of items""")
-    total: int = Field(None, description="""total number of items matching a query""")
+    limit: int = Field(..., description="""number of items to return in a response""")
+    offset: int = Field(..., description="""offset into the total number of items""")
+    total: int = Field(..., description="""total number of items matching a query""")
 
 
 class EntityResults(Results):
@@ -214,9 +239,9 @@ class EntityResults(Results):
         default_factory=list,
         description="""A collection of items, with the type to be overriden by slot_usage""",
     )
-    limit: int = Field(None, description="""number of items to return in a response""")
-    offset: int = Field(None, description="""offset into the total number of items""")
-    total: int = Field(None, description="""total number of items matching a query""")
+    limit: int = Field(..., description="""number of items to return in a response""")
+    offset: int = Field(..., description="""offset into the total number of items""")
+    total: int = Field(..., description="""total number of items matching a query""")
 
 
 class SearchResult(Entity):
@@ -225,9 +250,9 @@ class SearchResult(Entity):
         None, description="""matching text snippet containing html tags"""
     )
     score: Optional[float] = Field(None)
-    id: str = Field(None)
+    id: str = Field(...)
     category: List[str] = Field(default_factory=list)
-    name: str = Field(None)
+    name: str = Field(...)
     description: Optional[str] = Field(None)
     xref: Optional[List[str]] = Field(default_factory=list)
     provided_by: Optional[str] = Field(None)
@@ -252,27 +277,27 @@ class SearchResults(Results):
         default_factory=dict,
         description="""Collection of facet query responses with the query string values and counts""",
     )
-    limit: int = Field(None, description="""number of items to return in a response""")
-    offset: int = Field(None, description="""offset into the total number of items""")
-    total: int = Field(None, description="""total number of items matching a query""")
+    limit: int = Field(..., description="""number of items to return in a response""")
+    offset: int = Field(..., description="""offset into the total number of items""")
+    total: int = Field(..., description="""total number of items matching a query""")
 
 
 class FacetValue(ConfiguredBaseModel):
 
-    label: str = Field(None)
+    label: str = Field(...)
     count: Optional[int] = Field(None, description="""count of documents""")
 
 
 class HistoBin(FacetValue):
 
-    id: str = Field(None)
-    label: str = Field(None)
+    id: str = Field(...)
+    label: str = Field(...)
     count: Optional[int] = Field(None, description="""count of documents""")
 
 
 class FacetField(ConfiguredBaseModel):
 
-    label: str = Field(None)
+    label: str = Field(...)
     facet_values: Optional[Dict[str, FacetValue]] = Field(default_factory=dict)
 
 
@@ -307,7 +332,7 @@ class AssociationTypeMapping(ConfiguredBaseModel):
 class AssociationCount(FacetValue):
 
     association_type: Optional[AssociationTypeEnum] = Field(None)
-    label: str = Field(None)
+    label: str = Field(...)
     count: Optional[int] = Field(None, description="""count of documents""")
 
 
--- frontend/src/api/model.ts
+++ frontend/src/api/model.ts
@@ -1,68 +1,106 @@
-export type NodeId = string
-export type TaxonId = string
-export type AssociationId = string
-export type DirectionalAssociationId = string
-export type EntityId = string
-export type HistoPhenoId = string
-export type HistoBinId = string
-export type SearchResultId = string
-export type FacetValueLabel = string
-export type FacetFieldLabel = string
-export type AssociationCountLabel = string
+export type NodeId = string;
+export type TaxonId = string;
+export type AssociationId = string;
+export type DirectionalAssociationId = string;
+export type EntityId = string;
+export type HistoPhenoId = string;
+export type HistoBinId = string;
+export type SearchResultId = string;
+export type FacetValueLabel = string;
+export type FacetFieldLabel = string;
+export type AssociationCountLabel = string;
+/**
+* The directionality of an association as it relates to a specified entity, with edges being categorized as incoming or outgoing
+*/
+export enum AssociationDirectionEnum {
+    
+    /** An association for which a specified entity is the object or part of the object closure */
+    incoming = "incoming",
+    /** An association for which a specified entity is the subject or part of the subject closure */
+    outgoing = "outgoing",
+};
+/**
+* A grouping label for association types, which are not necessarily 1:1 with biolink categories or predicates
+*/
+export enum AssociationTypeEnum {
+    
+    /** Any association between a disease and a phenotype */
+    disease_phenotype = "disease_phenotype",
+    /** Any association between a gene and a phenotype */
+    gene_phenotype = "gene_phenotype",
+    /** Any association between two genes */
+    gene_interaction = "gene_interaction",
+    /** Any association between a gene and a pathway */
+    gene_pathway = "gene_pathway",
+    /** Expression association between a gene and an expression site */
+    gene_expression = "gene_expression",
+    /** Any association between two genes based on orthology */
+    gene_orthology = "gene_orthology",
+    /** Any association between a chemical and a pathway */
+    chemical_pathway = "chemical_pathway",
+    /** Any association between a gene and molecular activity */
+    gene_function = "gene_function",
+    /** Association between a gene and a disease that has not been established to be causal */
+    correlated_gene = "correlated_gene",
+    /** Association between a gene and a disease that is known to be causal */
+    causal_gene = "causal_gene",
+};
 
-export interface Node  extends Entity  {
+
+
+export interface Node extends Entity {
     taxon?: Taxon,
     inheritance?: Entity,
     association_counts?: AssociationCount[],
     node_hierarchy?: NodeHierarchy,
-    id?: string,
-    category?: string,
+    id: string,
+    category?: string[],
     name?: string,
     description?: string,
-    xref?: string,
+    xref?: string[],
     provided_by?: string,
     in_taxon?: string,
     source?: string,
     symbol?: string,
     type?: string,
-    synonym?: string,
-}
+    synonym?: string[],
+};
 
-export interface Taxon  {
-    id?: string,
-    label?: string,
-}
+export interface Taxon {
+    id: string,
+    label: string,
+};
 
-export interface NodeHierarchy  {
+export interface NodeHierarchy {
     super_classes?: Entity[],
     equivalent_classes?: Entity[],
     sub_classes?: Entity[],
-}
+};
 
-export interface Association  {
-    aggregator_knowledge_source?: string,
-    id?: string,
-    subject?: string,
+export interface Association {
+    aggregator_knowledge_source?: string[],
+    id: string,
+    subject: string,
     original_subject?: string,
     subject_namespace?: string,
-    subject_category?: string,
-    subject_closure?: string,
+    subject_category?: string[],
+    subject_closure?: string[],
     subject_label?: string,
-    subject_closure_label?: string,
-    predicate?: string,
-    object?: string,
+    subject_closure_label?: string[],
+    predicate: string,
+    object: string,
     original_object?: string,
     object_namespace?: string,
-    object_category?: string,
-    object_closure?: string,
+    object_category?: string[],
+    object_closure?: string[],
     object_label?: string,
-    object_closure_label?: string,
-    primary_knowledge_source?: string,
-    category?: string,
+    object_closure_label?: string[],
+    primary_knowledge_source?: string[],
+    category?: string[],
     negated?: boolean,
     provided_by?: string,
-    publications?: string,
-    qualifiers?: string,
+    publications?: string[],
+    qualifiers?: string[],
     frequency_qualifier?: string,
     has_evidence?: string,
     onset_qualifier?: string,
@@ -71,37 +109,36 @@ export interface Association  {
     stage_qualifier?: string,
     pathway?: string,
     relation?: string,
-}
+};
 /**
  * An association that gives it's direction relative to a specified entity
  */
-export interface DirectionalAssociation  extends Association  {
-    /**
-     * The directionality of the association relative to a given entity for an association_count. If the entity is the subject or in the subject closure, the direction is forwards, if it is the object or in the object closure, the direction is backwards.
-     */direction?: string,
-    aggregator_knowledge_source?: string,
-    id?: string,
-    subject?: string,
+export interface DirectionalAssociation extends Association {
+    /** The directionality of the association relative to a given entity for an association_count. If the entity is the subject or in the subject closure, the direction is forwards, if it is the object or in the object closure, the direction is backwards. */
+    direction: string,
+    aggregator_knowledge_source?: string[],
+    id: string,
+    subject: string,
     original_subject?: string,
     subject_namespace?: string,
-    subject_category?: string,
-    subject_closure?: string,
+    subject_category?: string[],
+    subject_closure?: string[],
     subject_label?: string,
-    subject_closure_label?: string,
-    predicate?: string,
-    object?: string,
+    subject_closure_label?: string[],
+    predicate: string,
+    object: string,
     original_object?: string,
     object_namespace?: string,
-    object_category?: string,
-    object_closure?: string,
+    object_category?: string[],
+    object_closure?: string[],
     object_label?: string,
-    object_closure_label?: string,
-    primary_knowledge_source?: string,
-    category?: string,
+    object_closure_label?: string[],
+    primary_knowledge_source?: string[],
+    category?: string[],
     negated?: boolean,
     provided_by?: string,
-    publications?: string,
-    qualifiers?: string,
+    publications?: string[],
+    qualifiers?: string[],
     frequency_qualifier?: string,
     has_evidence?: string,
     onset_qualifier?: string,
@@ -110,179 +147,147 @@ export interface DirectionalAssociation  extends Association  {
     stage_qualifier?: string,
     pathway?: string,
     relation?: string,
-}
+};
 
-export interface AssociationResults  extends Results  {
-    /**
-     * A collection of items, with the type to be overriden by slot_usage
-     */items?: Association[],
-    /**
-     * number of items to return in a response
-     */limit?: number,
-    /**
-     * offset into the total number of items
-     */offset?: number,
-    /**
-     * total number of items matching a query
-     */total?: number,
-}
+export interface AssociationResults extends Results {
+    /** A collection of items, with the type to be overriden by slot_usage */
+    items: Association[],
+    /** number of items to return in a response */
+    limit: number,
+    /** offset into the total number of items */
+    offset: number,
+    /** total number of items matching a query */
+    total: number,
+};
 
-export interface AssociationTableResults  extends Results  {
-    /**
-     * A collection of items, with the type to be overriden by slot_usage
-     */items?: DirectionalAssociation[],
-    /**
-     * number of items to return in a response
-     */limit?: number,
-    /**
-     * offset into the total number of items
-     */offset?: number,
-    /**
-     * total number of items matching a query
-     */total?: number,
-}
+export interface AssociationTableResults extends Results {
+    /** A collection of items, with the type to be overriden by slot_usage */
+    items: DirectionalAssociation[],
+    /** number of items to return in a response */
+    limit: number,
+    /** offset into the total number of items */
+    offset: number,
+    /** total number of items matching a query */
+    total: number,
+};
 
-export interface Entity  {
-    id?: string,
-    category?: string,
+export interface Entity {
+    id: string,
+    category?: string[],
     name?: string,
     description?: string,
-    xref?: string,
+    xref?: string[],
     provided_by?: string,
     in_taxon?: string,
     source?: string,
     symbol?: string,
     type?: string,
-    synonym?: string,
-}
+    synonym?: string[],
+};
 
-export interface EntityResults  extends Results  {
-    /**
-     * A collection of items, with the type to be overriden by slot_usage
-     */items?: Entity[],
-    /**
-     * number of items to return in a response
-     */limit?: number,
-    /**
-     * offset into the total number of items
-     */offset?: number,
-    /**
-     * total number of items matching a query
-     */total?: number,
-}
+export interface EntityResults extends Results {
+    /** A collection of items, with the type to be overriden by slot_usage */
+    items: Entity[],
+    /** number of items to return in a response */
+    limit: number,
+    /** offset into the total number of items */
+    offset: number,
+    /** total number of items matching a query */
+    total: number,
+};
 
-export interface HistoPheno  {
-    id?: string,
-    /**
-     * A collection of items, with the type to be overriden by slot_usage
-     */items?: HistoBin[],
-}
+export interface HistoPheno {
+    id: string,
+    /** A collection of items, with the type to be overriden by slot_usage */
+    items: HistoBin[],
+};
 
-export interface HistoBin  extends FacetValue  {
-    id?: string,
-    label?: string,
-    /**
-     * count of documents
-     */count?: number,
-}
+export interface HistoBin extends FacetValue {
+    id: string,
+    label: string,
+    /** count of documents */
+    count?: number,
+};
 
-export interface Results  {
-    /**
-     * number of items to return in a response
-     */limit?: number,
-    /**
-     * offset into the total number of items
-     */offset?: number,
-    /**
-     * total number of items matching a query
-     */total?: number,
-}
+export interface Results {
+    /** number of items to return in a response */
+    limit: number,
+    /** offset into the total number of items */
+    offset: number,
+    /** total number of items matching a query */
+    total: number,
+};
 
-export interface SearchResult  extends Entity  {
-    /**
-     * matching text snippet containing html tags
-     */highlight?: string,
+export interface SearchResult extends Entity {
+    /** matching text snippet containing html tags */
+    highlight?: string,
     score?: number,
-    id?: string,
-    category?: string,
-    name?: string,
+    id: string,
+    category: string[],
+    name: string,
     description?: string,
-    xref?: string,
+    xref?: string[],
     provided_by?: string,
     in_taxon?: string,
     source?: string,
     symbol?: string,
     type?: string,
-    synonym?: string,
-}
+    synonym?: string[],
+};
 
-export interface SearchResults  extends Results  {
-    /**
-     * A collection of items, with the type to be overriden by slot_usage
-     */items?: SearchResult[],
-    /**
-     * Collection of facet field responses with the field values and counts
-     */facet_fields?: {[index: FacetFieldLabel]: FacetField },
-    /**
-     * Collection of facet query responses with the query string values and counts
-     */facet_queries?: {[index: FacetValueLabel]: FacetValue },
-    /**
-     * number of items to return in a response
-     */limit?: number,
-    /**
-     * offset into the total number of items
-     */offset?: number,
-    /**
-     * total number of items matching a query
-     */total?: number,
-}
+export interface SearchResults extends Results {
+    /** A collection of items, with the type to be overriden by slot_usage */
+    items: SearchResult[],
+    /** Collection of facet field responses with the field values and counts */
+    facet_fields?: {[index: FacetFieldLabel]: FacetField },
+    /** Collection of facet query responses with the query string values and counts */
+    facet_queries?: {[index: FacetValueLabel]: FacetValue },
+    /** number of items to return in a response */
+    limit: number,
+    /** offset into the total number of items */
+    offset: number,
+    /** total number of items matching a query */
+    total: number,
+};
 
-export interface FacetValue  {
-    label?: string,
-    /**
-     * count of documents
-     */count?: number,
-}
+export interface FacetValue {
+    label: string,
+    /** count of documents */
+    count?: number,
+};
 
-export interface FacetField  {
-    label?: string,
+export interface FacetField {
+    label: string,
     facet_values?: {[index: FacetValueLabel]: FacetValue },
-}
+};
 /**
  * A data class to hold the necessary information to produce association type counts for given  entities with appropriate directional labels
  */
-export interface AssociationTypeMapping  {
+export interface AssociationTypeMapping {
     association_type?: string,
-    /**
-     * A label to describe the subjects of the association type as a whole for use in the UI
-     */subject_label?: string,
-    /**
-     * A label to describe the objects of the association type as a whole for use in the UI
-     */object_label?: string,
-    /**
-     * Whether the association type is symmetric, meaning that the subject and object labels should be interchangeable
-     */symmetric?: boolean,
-    /**
-     * The biolink categories to use in queries for this association type, assuming OR semantics
-     */category?: string,
-    /**
-     * The biolink predicate to use in queries for this association type, assuming OR semantics
-     */predicate?: string,
-}
+    /** A label to describe the subjects of the association type as a whole for use in the UI */
+    subject_label?: string,
+    /** A label to describe the objects of the association type as a whole for use in the UI */
+    object_label?: string,
+    /** Whether the association type is symmetric, meaning that the subject and object labels should be interchangeable */
+    symmetric: boolean,
+    /** The biolink categories to use in queries for this association type, assuming OR semantics */
+    category?: string[],
+    /** The biolink predicate to use in queries for this association type, assuming OR semantics */
+    predicate: string[],
+};
 
-export interface AssociationCount  extends FacetValue  {
+export interface AssociationCount extends FacetValue {
     association_type?: string,
-    label?: string,
-    /**
-     * count of documents
-     */count?: number,
-}
+    label: string,
+    /** count of documents */
+    count?: number,
+};
 /**
  * Container class for a list of association counts
  */
-export interface AssociationCountList  {
-    /**
-     * A collection of items, with the type to be overriden by slot_usage
-     */items?: AssociationCount[],
-}
+export interface AssociationCountList {
+    /** A collection of items, with the type to be overriden by slot_usage */
+    items: AssociationCount[],
+};
 
