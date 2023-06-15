312ab73b261f86131ed8ee60a5b6af090f15ec9a
glass-ships <glass.ships@outlook.com>
2023-06-15 15:31:07 -0600

    start allowing multiple values for some association args

--- backend/src/monarch_api/association.py
+++ backend/src/monarch_api/association.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends
+from typing import List, Union
+from typing_extensions import Annotated
+
+from fastapi import APIRouter, Depends, Query
 from monarch_api.additional_models import PaginationParams
 from monarch_api.config import settings
 from monarch_api.model import AssociationResults
@@ -10,32 +13,18 @@ router = APIRouter(
 )
 
 
-@router.get("/all")
+@router.get("")
 async def _get_all_associations(
+    category: Union[List[str], None] = Query(default=None),
+    subject: Union[List[str], None] = Query(default=None),
+    predicate: Union[List[str], None] = Query(default=None),
+    object: Union[List[str], None] = Query(default=None),
+    entity: Union[List[str], None] = Query(default=None),
+    direct: Union[bool, None] = Query(default=None),
     pagination: PaginationParams = Depends(),
-    category: str = None,
-    predicate: str = None,
-    subject: str = None,
-    object: str = None,
-    entity: str = None,  # return nodes where entity is subject or object
-    between: str = None,  # strip by comma and check associations in both directions
-    association_type: str = None,
 ) -> AssociationResults:
-    """Retrieves all associations for a given entity, or between two entities
+    """Retrieves all associations for a given entity, or between two entities."""
 
-    Args:
-        pagination (PaginationParams, optional): _description_. Defaults to Depends().
-        category (str, optional): _description_. Defaults to None.
-        predicate (str, optional): _description_. Defaults to None.
-        subject (str, optional): _description_. Defaults to None.
-        object (str, optional): _description_. Defaults to None.
-        entity (str, optional): _description_. Defaults to None.
-        between (str, optional): A comma separated pair of entites associated in either direction.
-        association_type (str, optional): An association type from AssociationTypeEnum in the model. Defaults to None.
-
-    Returns:
-        AssociationResults: _description_
-    """
     si = SolrImplementation(base_url=settings.solr_url)
     response = si.get_associations(
         category=category,
@@ -43,72 +32,9 @@ async def _get_all_associations(
         subject=subject,
         object=object,
         entity=entity,
-        between=between,
-        association_type=association_type,
+        direct=direct,
         offset=pagination.offset,
         limit=pagination.limit,
-    )
-
-    return response
-
-
-@router.get("/to/{subject}")
-async def _get_association_to(
-    subject: str, pagination: PaginationParams = Depends()
-) -> AssociationResults:
-    """Retrieves all associations to an entity as the subject
-
-    Args:
-        subject (str): _description_
-        pagination (PaginationParams, optional): _description_. Defaults to Depends().
-
-    Returns:
-        AssociationResults: _description_
-    """
-    si = SolrImplementation(base_url=settings.solr_url)
-    response = si.get_associations(
-        subject=subject, offset=pagination.offset, limit=pagination.limit
-    )
-
-    return response
-
-
-@router.get("/from/{object}")
-async def _get_association_from(object: str, pagination: PaginationParams = Depends()):
-    """Retrieves all associations from an entity as the object
-
-    Args:
-        object (str): _description_
-        pagination (PaginationParams, optional): _description_. Defaults to Depends().
-
-    Returns:
-        _type_: _description_
-    """
-    si = SolrImplementation(base_url=settings.solr_url)
-    response = si.get_associations(
-        object=object, offset=pagination.offset, limit=pagination.limit
-    )
-
-    return response
-
-
-@router.get("/between/{subject}/{object}")
-async def _get_association_between(
-    subject: str, object: str, pagination: PaginationParams = Depends()
-) -> AssociationResults:
-    """Retrieves all associations from an object to a subject
-
-    Args:
-        subject (str): _description_
-        object (str): _description_
-        pagination (PaginationParams, optional): _description_. Defaults to Depends().
-
-    Returns:
-        AssociationResults: _description_
-    """
-    si = SolrImplementation(base_url=settings.solr_url)
-    response = si.get_associations(
-        subject=subject, object=object, offset=pagination.offset, limit=pagination.limit
     )
 
     return response
