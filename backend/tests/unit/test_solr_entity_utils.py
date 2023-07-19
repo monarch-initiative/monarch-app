b5ab2cd6a317356e6bc07b1e0c5dcd694cff3c18
Kevin Schaper <kevinschaper@gmail.com>
2023-07-19 14:44:40 -0700

    Fix subclass section linking to self (#236)
    Stops the code from using the subject or object closure for super class
    and sub class check, just direct associations
    
    Fixes #190

--- backend/tests/unit/test_solr_entity_utils.py
+++ backend/tests/unit/test_solr_entity_utils.py
@@ -4,42 +4,44 @@ from monarch_py.implementations.solr.solr_implementation import SolrImplementati
 
 
 @pytest.mark.parametrize(
-    "association",
+    ("association", "this_entity", "other_entity"),
     [
-        Association(
-            id="uuid:0000001",
-            subject="ENT:0000003",
-            subject_closure=["ENT:0000001"],
-            object="ENT:0000002",
-            object_category="biolink:testCase",
-            object_label="Test Case 2",
-            predicate="ASSOC:0000001",
+        (
+            Association(
+                id="uuid:0000001",
+                subject="ENT:0000003",
+                subject_category="biolink:testCase",
+                subject_label="Test Case 3",
+                subject_closure=["ENT:0000001"],
+                object="ENT:0000002",
+                object_category="biolink:testCase",
+                object_label="Test Case 2",
+                predicate="biolink:subclass_of",
+            ),
+            Entity(id="ENT:0000002", name="Test Case 2", category="biolink:testCase"),
+            Entity(id="ENT:0000003", name="Test Case 3", category="biolink:testCase"),
         ),
-        Association(
-            id="uuid:0000002",
-            object="ENT:0000004",
-            object_closure=["ENT:0000001"],
-            subject="ENT:0000002",
-            subject_category="biolink:testCase",
-            subject_label="Test Case 2",
-            predicate="ASSOC:0000002",
+        (
+            Association(
+                id="uuid:0000002",
+                object="ENT:0000004",
+                object_category="biolink:testCase",
+                object_label="Test Case 4",
+                object_closure=["ENT:0000001"],
+                subject="ENT:0000003",
+                subject_category="biolink:testCase",
+                subject_label="Test Case 3",
+                predicate="biolink:subclass_of",
+            ),
+            Entity(id="ENT:0000003", name="Test Case 3", category="biolink:testCase"),
+            Entity(id="ENT:0000004", name="Test Case 4", category="biolink:testCase"),
         ),
     ],
 )
-def test_get_associated_entity(association):
+def test_get_associated_entity(association, this_entity, other_entity):
     """Test that the get_associated_entity function returns the correct entity"""
-    entity_1 = Entity(
-        id="ENT:0000001",
-        name="Test Case 1",
-        category="biolink:testCase",
-    )
-    entity_2 = Entity(
-        id="ENT:0000002",
-        name="Test Case 2",
-        category="biolink:testCase",
-    )
     si = SolrImplementation()
-    associated_entity = si._get_associated_entity(association, entity_1)
+    associated_entity = si._get_associated_entity(association, this_entity)
     assert (
-        associated_entity == entity_2
-    ), f"Associated entity is not as expected. Expected: {entity_2}, got: {associated_entity}"
+        associated_entity == other_entity
+    ), f"Associated entity is not as expected. Expected: {other_entity}, got: {associated_entity}"
