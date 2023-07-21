00af29f7720e4dacf8330b4709ea0b6b3d9f3534
glass-ships <glass.ships@outlook.com>
2023-07-21 12:21:31 -0600

    switch to semsimian

--- backend/src/monarch_py/implementations/oak/oak_implementation.py
+++ backend/src/monarch_py/implementations/oak/oak_implementation.py
@@ -1,32 +1,17 @@
 from dataclasses import dataclass
-# from typing import Iterable, List
 
 from oaklib.interfaces.semsim_interface import SemanticSimilarityInterface
+from oaklib.datamodels.similarity import TermSetPairwiseSimilarity
 from oaklib.selector import get_adapter
 
 
 @dataclass
-class OakImplementation(SemanticSimilarityInterface):
-    """Implementation of Monarch Interfaces for OAK
-    Notes:
-        - This is an in-progress conversion to OAK
-        - Biolink-API call args:
-            - reference_ids -> subject_ids
-            - query_ids -> object_ids
-            - is_feature_set = ??
-    """
-    oi = get_adapter(f"sqlite:obo:phenio") 
+# class OakImplementation(SemanticSimilarityInterface):
+class OakImplementation():
+    """Implementation of Monarch Interfaces for OAK"""
 
+    semsim = get_adapter(f"semsimian:sqlite:obo:phenio") 
 
-    def termset_pairwise_similarity(self, ts1, ts2, predicates=None, labels=False) -> float:
-        tsps = self.oi.termset_pairwise_similarity(ts1, ts2, predicates=predicates, labels=labels)
-
-        average_score = tsps["average_score"]
-        best_score = tsps["best_score"]
-        subject_termset = tsps["subject_termset"]
-        object_termset = tsps["object_termset"]
-        subject_best_matches = tsps["subject_best_matches"]
-        object_best_matches = tsps["object_best_matches"]
-
-        return tsps
-
+    def compare(self, ts1, ts2, predicates=None, labels=False) -> TermSetPairwiseSimilarity:
+        """Compare two sets of terms using OAK"""
+        return self.semsim.termset_pairwise_similarity(ts1, ts2, predicates=predicates, labels=labels)
--- backend/tests/unit/test_oak_semsim.py
+++ backend/tests/unit/test_oak_semsim.py
@@ -2,17 +2,17 @@ import pytest
 
 from monarch_py.implementations.oak.oak_implementation import OakImplementation
 from monarch_py.utils.utils import dict_diff, compare_dicts
-# from profiler import profile
+from profiler import profile
 
-# @profile()
+@profile()
 # @pytest.mark.skip(reason="This is a long running test")
-def test_oak():
+def test_oak_semsim_compare():
 
     subject_ids = ["MP:0010771", "MP:0002169", "MP:0005391", "MP:0005389", "MP:0005367"]
     object_ids = ["HP:0004325", "HP:0000093", "MP:0006144"]
 
     oi = OakImplementation()
-    tsps = oi.termset_pairwise_similarity(subject_ids, object_ids)
+    tsps = oi.compare(subject_ids, object_ids)
 
     import pprint
     pp = pprint.PrettyPrinter(indent=2)
@@ -20,4 +20,4 @@ def test_oak():
 
 
 if __name__ == "__main__":
-    test_oak()
+    test_oak_semsim_compare()
