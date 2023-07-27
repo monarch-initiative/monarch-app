685e5e30a78a834ea6aebd92c4719e0301722f4c
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-07-27 19:19:52 -0400

    rework icon component, change svg inline method

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import svgLoader from "vite-svg-loader";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader({ svgo: false })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
