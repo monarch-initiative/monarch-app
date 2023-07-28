90d5fd8d473ad3615284bdb7a8166afeb45dd75a
kevinschaper <kevinschaper@gmail.com>
2023-07-28 22:54:01 +0000

    Deploying to gh-pages from @ monarch-initiative/monarch-app@e6638e60fc0e40214b06715ad5c78a24acd12173 🚀

---
+++ assets/category-disease-f4af825f.js
@@ -0,0 +1,1 @@
+import{h as t,i as o,j as e}from"./index-fa6cdfbb.js";const s={xmlns:"http://www.w3.org/2000/svg",viewBox:"-10 -10 120 120"},c=e("circle",{cx:"50",cy:"50",r:"55"},null,-1),n=e("path",{d:"M70 80C25 50 30 17 50 17s25 33-20 63"},null,-1),r=[c,n];function a(i,l){return t(),o("svg",s,r)}const d={render:a};export{d as default,a as render};
