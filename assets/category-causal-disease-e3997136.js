9decab6365e3ef8bc76e82e67faf1680df8022d3
kevinschaper <kevinschaper@gmail.com>
2023-07-28 22:02:05 +0000

    Deploying to gh-pages from @ monarch-initiative/monarch-app@3f457dbe811629f9b5b6a0621e7ab9bbdfcd47c8 🚀

---
+++ assets/category-causal-disease-e3997136.js
@@ -0,0 +1,1 @@
+import{h as t,i as s,j as e}from"./index-4ae9a30b.js";const o={xmlns:"http://www.w3.org/2000/svg",viewBox:"-10 -10 120 120"},c=e("circle",{cx:"50",cy:"50",r:"55"},null,-1),n=e("path",{d:"M70 80C25 50 30 17 50 17s25 33-20 63"},null,-1),r=[c,n];function a(l,i){return t(),s("svg",o,r)}const d={render:a};export{d as default,a as render};
---
+++ assets/category-correlated-disease-e8e0a10f.js
@@ -0,0 +1,1 @@
+import{h as t,i as o,j as e}from"./index-4ae9a30b.js";const s={xmlns:"http://www.w3.org/2000/svg",viewBox:"-10 -10 120 120"},c=e("circle",{cx:"50",cy:"50",r:"55"},null,-1),r=e("path",{d:"M70 80C25 50 30 17 50 17s25 33-20 63"},null,-1),n=[c,r];function a(l,d){return t(),o("svg",s,n)}const _={render:a};export{_ as default,a as render};
