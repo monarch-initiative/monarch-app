dee47363efd0153a1c2e7205b450663a3afa583a
glass-ships <glass.ships@outlook.com>
2023-08-29 22:27:25 +0000

    Deploying to gh-pages from @ monarch-initiative/monarch-app@eb23f3a333e87836bd12ceaa421a11186bb82cda 🚀

---
+++ assets/category-correlated-disease-744e4557.js
@@ -0,0 +1,1 @@
+import{h as o,o as t,j as e}from"./index-8b11ae4e.js";const r={xmlns:"http://www.w3.org/2000/svg",viewBox:"-10 -10 120 120",fill:"none",stroke:"currentColor"},s=e("circle",{cx:"50",cy:"50",r:"55"},null,-1),c=e("path",{d:"M70 80C25 50 30 17 50 17s25 33-20 63"},null,-1),n=[s,c];function l(a,d){return t(),o("svg",r,n)}const _={render:l};export{_ as default,l as render};
