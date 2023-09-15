5a310520992461d023a86e9b927fcd172e992bc2
Kevin Schaper <kevinschaper@gmail.com>
2023-09-15 07:49:47 -0700

    Support for getting phenio.db location from the environment

--- backend/src/monarch_py/api/config.py
+++ backend/src/monarch_py/api/config.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     solr_host = os.getenv("SOLR_HOST") if os.getenv("SOLR_HOST") else "127.0.0.1"
     solr_port = os.getenv("SOLR_PORT") if os.getenv("SOLR_PORT") else 8983
     solr_url = os.getenv("SOLR_URL") if os.getenv("SOLR_URL") else f"http://{solr_host}:{solr_port}/solr"
+    phenio_db_path = os.getenv("PHENIO_DB_PATH") if os.getenv("PHENIO_DB_PATH") else "/data/phenio.db"
 
 
 settings = Settings()
