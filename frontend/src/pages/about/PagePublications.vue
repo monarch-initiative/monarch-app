0554e6b11df62b66b9f1c0fa51c98f007452f064
glass-ships <glass.ships@outlook.com>
2023-09-15 18:07:35 -0600

    update title for pubs page

--- frontend/src/pages/about/PagePublications.vue
+++ frontend/src/pages/about/PagePublications.vue
@@ -6,15 +6,13 @@
 
 <template>
   <AppSection>
-    <AppHeading>Publications</AppHeading>
+    <AppHeading>Monarch-Related Publications</AppHeading>
 
-    <Apex
-      class="chart"
-      type="bar"
-      :options="options"
-      :series="[citesPerYear]"
-      height="300px"
-    />
+    <p class="metadata">
+      This list includes papers by the Monarch Team that were foundational to the current Monarch work. The graph below shows the number of citations to Monarch papers over time.
+    </p>
+
+    <Apex class="chart" type="bar" :options="options" :series="[citesPerYear]" height="300px" />
 
     <AppGallery>
       <p v-for="(item, index) in metadata" :key="index" class="metadata">
@@ -29,10 +27,7 @@
     <AppHeading>Years</AppHeading>
     <!-- row of links to year sections -->
     <p>
-      <template
-        v-for="(group, index) in publications.publications"
-        :key="index"
-      >
+      <template v-for="(group, index) in publications.publications" :key="index">
         <AppLink :to="'#' + group.year">{{ group.year }}</AppLink>
         <span v-if="index !== publications.publications.length - 1"> · </span>
       </template>
@@ -40,11 +35,7 @@
   </AppSection>
 
   <!-- by year -->
-  <AppSection
-    v-for="(group, index) in publications.publications"
-    :key="index"
-    width="big"
-  >
+  <AppSection v-for="(group, index) in publications.publications" :key="index" width="big">
     <AppHeading>{{ group.year }}</AppHeading>
     <AppGallery>
       <AppCitation
@@ -69,15 +60,13 @@ import publications from "./publications.json";
 /** data for chart */
 const citesPerYear = {
   name: "citations",
-  data: Object.entries(publications.metadata.cites_per_year).map(
-    ([year, count]) => ({ x: year, y: count }),
-  ),
+  data: Object.entries(publications.metadata.cites_per_year).map(([year, count]) => ({ x: year, y: count })),
 };
 
 /** extra metadata fields */
 const metadata: {
   name: string;
-  key: keyof (typeof publications)["metadata"];
+  key: keyof typeof publications["metadata"];
 }[] = [
   { name: "Total publications", key: "num_publications" },
   { name: "Total citations", key: "total" },
