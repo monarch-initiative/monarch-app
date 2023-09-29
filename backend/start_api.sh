2b5b5bf8099c30cb7d4c6ed3efdbe2449a60f7fb
Glass <glass.ships@outlook.com>
2023-09-29 13:48:13 -0600

    Update backend/start_api.sh
    Co-authored-by: Faisal Alquaddoomi <falquaddoomi@gmail.com>

--- backend/start_api.sh
+++ backend/start_api.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash 
 
 # check for uvicorn workers env var, default to 1
-if [ -z "${UVICORN_WORKERS}" ]; then
-    UVICORN_WORKERS=1
-fi
+UVICORN_WORKERS=${UVICORN_WORKERS:-1}
 
 # Start the API
 /opt/poetry/bin/poetry run uvicorn src.monarch_py.api.main:app --host 0.0.0.0 --port 8000 --workers ${UVICORN_WORKERS}
