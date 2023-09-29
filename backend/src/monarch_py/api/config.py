92982437017f28951079b4027a240648a41a3271
Kevin Schaper <kevinschaper@gmail.com>
2023-09-29 15:34:37 -0700

    Load phenio.db from /data if available, use pystow to fetch from monarch data bucket if not (#356)
    This should stop our multiple pystow download problem (and mean no
    download on startup at all, happily)

--- backend/src/monarch_py/api/config.py
+++ backend/src/monarch_py/api/config.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     solr_host = os.getenv("SOLR_HOST") if os.getenv("SOLR_HOST") else "127.0.0.1"
     solr_port = os.getenv("SOLR_PORT") if os.getenv("SOLR_PORT") else 8983
     solr_url = os.getenv("SOLR_URL") if os.getenv("SOLR_URL") else f"http://{solr_host}:{solr_port}/solr"
+    phenio_db_path = os.getenv("PHENIO_DB_PATH") if os.getenv("PHENIO_DB_PATH") else "/data/phenio.db"
 
 
 settings = Settings()
@@ -23,4 +24,5 @@ def solr():
 
 @lru_cache(maxsize=1)
 def oak():
-    return OakImplementation().init_semsim()
+    phenio_db_path = settings.phenio_db_path if os.path.exists(settings.phenio_db_path) else None
+    return OakImplementation().init_semsim(phenio_path=phenio_db_path)
