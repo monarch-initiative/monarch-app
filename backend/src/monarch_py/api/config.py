fe96b950eccaea21fa2f4a649c8ad93c6e31beda
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2023-10-05 20:21:14 -0600

    Removes OAK RPC server and dependencies. config.oak().compare() now queries semsimian-server over HTTP and (for now) transforms the result into something that TermSetPairwiseSimilarity() can interpret.

--- backend/src/monarch_py/api/config.py
+++ backend/src/monarch_py/api/config.py
@@ -1,12 +1,9 @@
 import os
-import zmq
+import requests as rq
+
 from functools import lru_cache
 
 from pydantic import BaseSettings
-
-from tinyrpc import RPCClient
-from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
-from tinyrpc.transports.zmq import ZmqClientTransport
 
 from monarch_py.implementations.oak.oak_implementation import OakImplementation
 from monarch_py.implementations.solr.solr_implementation import SolrImplementation
@@ -30,34 +27,63 @@ settings = Settings()
 def solr():
     return SolrImplementation(settings.solr_url)
 
-class OakRPCMarshaller:
-    def __init__(self) -> None:
-        pass
 
-    def connect_to_oak(self):
-        ctx = zmq.Context()
-        rpc_client = RPCClient(
-            JSONRPCProtocol(),
-            ZmqClientTransport.create(
-                ctx, f'tcp://{settings.oak_server_host}:{settings.oak_server_port}'
-            )
+def convert_nans(input_dict, to_value=None):
+    """
+    Given an input dict of the form {<term>: {<field>: <value>, ...}}
+    converts any <value> of 'NaN' to None.
+    """
+    for k, v in input_dict.items():
+        for ik, iv in v.items():
+            if iv == 'NaN':
+                input_dict[k][ik] = None
+
+    return input_dict
+
+
+class OakHTTPRequester:
+    def compare(self, subjects, objects):
+        host = f"http://{settings.oak_server_host}:{settings.oak_server_port}"
+        path = f"/compare/{','.join(subjects)}/{','.join(objects)}"
+        url = f"{host}/{path}"
+
+        print(f"Fetching {url}...")
+        response = rq.get(url=url)
+        data = response.json()
+
+        # FIXME: currently, the response returned from semsimian_server doesn't
+        #  100% match the TermSetPairwiseSimilarity model, so we perform some
+        #  transformations below. once it does, we can remove all the code below
+        #  and just return TermSetPairwiseSimilarity(**data)
+
+        # remove these similarity maps and fold them into the _best_matches dicts
+        object_best_matches_similarity_map = convert_nans(
+            data.pop('object_best_matches_similarity_map')
         )
-        return rpc_client.get_proxy()
+        subject_best_matches_similarity_map = convert_nans(
+            data.pop('subject_best_matches_similarity_map')
+        )
 
-    def compare(self, *args, **kwargs):
-        # make the connection each time to the backend server, to get around it timing out
-        # (note to future readers: if we find this to be too expensive, we can
-        # try the existing connection and reconnect if there's an exception)
-        self.oak_server = self.connect_to_oak()
+        # convert to a format that can be coerced into a TermSetPairwiseSimilarity
+        converted_data = {
+            **data,
+            **{
+                'subject_termset': data['subject_termset'][0],
+                'object_termset': data['object_termset'][0],
+                'subject_best_matches': {
+                    k: {**v, 'similarity': subject_best_matches_similarity_map[k]}
+                    for k, v in data['subject_best_matches'].items()
+                },
+                'object_best_matches': {
+                    k: {**v, 'similarity': object_best_matches_similarity_map[k]}
+                    for k, v in data['object_best_matches'].items()
+                }
+            }
+        }
 
-        # for some reason TermSetPairwiseSimilarity is not JSON-serializable so
-        # we can't call compare() directly. instead, we call compare_as_dict(),
-        # which sends back a dict, and then we convert it into the expected
-        # TermSetPairwiseSimilarity type
-        result = self.oak_server.compare_as_dict(*args, **kwargs)
-        return TermSetPairwiseSimilarity(**result)
+        return TermSetPairwiseSimilarity(**converted_data)
 
 
 @lru_cache(maxsize=1)
 def oak():
-    return OakRPCMarshaller()
+    return OakHTTPRequester()
