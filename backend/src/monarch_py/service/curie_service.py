07f8424549b5c88b8a37be4310453573beae1cdc
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-10-10 16:05:27 -0700

    turn prefixmaps merged CSV into an extended prefix map, then return a converter

--- backend/src/monarch_py/service/curie_service.py
+++ backend/src/monarch_py/service/curie_service.py
@@ -2,6 +2,30 @@
 
 import curies
 from curies import Converter
+import requests
+import csv
+
+
+def from_csv_url(url):
+    response = requests.get(url)
+    response.raise_for_status()  # This will raise an exception if there's a download error
+
+    csv_data = response.content.decode('utf-8').splitlines()
+    csv_reader = csv.DictReader(csv_data)
+
+    prefix_map = []
+    for row in csv_reader:
+        # Assuming the CSV columns match the desired dictionary keys
+        # Adjust as needed if the CSV column names are different
+        entry = {
+            "prefix": row[1],
+            "uri_prefix": row[2],
+            "prefix_synonyms": [],
+            "uri_prefix_synonyms": []
+        }
+        prefix_map.append(entry)
+
+    return prefix_map
 
 
 class CurieService:
@@ -15,7 +39,11 @@ class CurieService:
         return cls._instance
 
     def initialize(self):
-        self.converter = curies.get_monarch_converter()
+        url = (
+            "https://raw.githubusercontent.com/linkml/prefixmaps/main/src/prefixmaps/data/merged.csv"
+        )
+
+        self.converter = Converter.from_extended_prefix_map(from_csv_url(url))
 
     def expand(self, curie: str) -> str:
         return self.converter.expand(curie)
