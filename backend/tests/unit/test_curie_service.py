09e2d28f51b4a2355c4a060014db3725f2332337
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-10-11 16:15:18 -0700

    more expansion testing

--- backend/tests/unit/test_curie_service.py
+++ backend/tests/unit/test_curie_service.py
@@ -11,9 +11,19 @@ import pytest
      ("NCBIGene:1017", "https://identifiers.org/ncbigene/1017"),
      ("ZFIN:ZDB-GENE-980526-166", "https://identifiers.org/zfin/ZDB-GENE-980526-166"),
      ("MGI:1918910", "https://identifiers.org/MGI/1918910"),
-     ("ENSEMBL:ENSG00000157764", "http://identifiers.org/ensembl/ENSG00000157764")]
+     ("ENSEMBL:ENSG00000157764", "http://identifiers.org/ensembl/ENSG00000157764"),
+     ("HP:0000001", "http://purl.obolibrary.org/obo/HP_0000001"),
+     ("UBERON:0000001", "http://purl.obolibrary.org/obo/UBERON_0000001"),
+     ("GO:0000001", "http://purl.obolibrary.org/obo/GO_0000001"),
+     ("CL:0000001", "http://purl.obolibrary.org/obo/CL_0000001"),
+     ("WormBase:WBGene00000001", "http://identifiers.org/wb/WBGene00000001"),
+     ("WB:WBGene00000001", "https://identifiers.org/wb/WBGene00000001"),
+     ("SGD:S000000001", "https://identifiers.org/sgd/S000000001"),
+     ("RGD:1", "http://identifiers.org/rgd/1"),
+     ("HGNC:5", "http://identifiers.org/hgnc/HGNC:5")]
 )
 def test_curie_expansion(curie, expanded_curie_part):
     cs = CurieService()
     expanded_curie = cs.expand(curie)
+    # print(expanded_curie, expanded_curie_part)
     assert expanded_curie_part in expanded_curie
