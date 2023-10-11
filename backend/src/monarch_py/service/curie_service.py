0ea542e2e06980f3dc05f89fa482aeca4cefdcd2
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-10-11 16:04:21 -0700

    more tests

--- backend/src/monarch_py/service/curie_service.py
+++ backend/src/monarch_py/service/curie_service.py
@@ -1,7 +1,7 @@
 ### make a singleton class that uses prefixmap and curies to expand curies
 
 from curies import Converter
-from prefixmaps import load_context
+from prefixmaps.io.parser import load_multi_context, load_context
 
 
 class CurieService:
@@ -17,7 +17,7 @@ class CurieService:
     def initialize(self):
         # this is a magic keyword that represents the "merged" context from Chris M's algorithm
         # (https://github.com/linkml/prefixmaps/blob/main/src/prefixmaps/data/merged.csv)
-        context = load_context("merged")
+        context = load_multi_context(["merged"])
         extended_prefix_map = context.as_extended_prefix_map()
         self.converter = Converter.from_extended_prefix_map(extended_prefix_map)
 
--- backend/tests/unit/test_curie_service.py
+++ backend/tests/unit/test_curie_service.py
@@ -4,8 +4,14 @@ import pytest
 
 @pytest.mark.parametrize(
     ("curie", "expanded_curie_part"),
-    [("FB:FBgn0000008", "flybase.org"),
-     ("FlyBase:FBgn0000008", "flybase.org")],
+    [("FB:FBgn0000008", "http://identifiers.org/flybase/FBgn0000008"), # this is a little odd, fb vs. flybase, but both resolve
+     ("FlyBase:FBgn0000008", "http://identifiers.org/fb/FBgn0000008"),
+     ("MONDO:0005737", "http://purl.obolibrary.org/obo/MONDO_0005737"),
+     ("OMIM:613647", "http://identifiers.org/mim/613647"),
+     ("NCBIGene:1017", "https://identifiers.org/ncbigene/1017"),
+     ("ZFIN:ZDB-GENE-980526-166", "https://identifiers.org/zfin/ZDB-GENE-980526-166"),
+     ("MGI:1918910", "https://identifiers.org/MGI/1918910"),
+     ("ENSEMBL:ENSG00000157764", "http://identifiers.org/ensembl/ENSG00000157764")]
 )
 def test_curie_expansion(curie, expanded_curie_part):
     cs = CurieService()
