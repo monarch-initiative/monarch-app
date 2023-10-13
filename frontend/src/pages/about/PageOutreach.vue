1c0aa06d77eb285ae493c9a67e2e8f2cd08ad187
glass-ships <glass.ships@outlook.com>
2023-10-12 18:11:19 -0600

    format

--- frontend/src/pages/about/PageOutreach.vue
+++ frontend/src/pages/about/PageOutreach.vue
@@ -6,51 +6,95 @@
   <AppSection>
     <AppHeading>Outreach</AppHeading>
     <p>
-      The Monarch Initiative is a community effort, and we welcome your participation!
+      The Monarch Initiative is a community effort, and we welcome your
+      participation!
     </p>
     <AppSection>
       <AppHeading>Ways to Get Involved</AppHeading>
 
       <ul>
         <li>
-          <a href="https://oboacademy.github.io/obook/courses/monarch-obo-training/" target="_blank">OBO Academy: Monarch Training Series</a>
+          <a
+            href="https://oboacademy.github.io/obook/courses/monarch-obo-training/"
+            target="_blank"
+            >OBO Academy: Monarch Training Series</a
+          >
           <p>
-            Training for Semantic Engineers, Ontology Curators, and Knowledge Scientists on basic technologies in and around the Monarch Initiative and Open Biological and Biomedical Ontologies (OBO).
+            Training for Semantic Engineers, Ontology Curators, and Knowledge
+            Scientists on basic technologies in and around the Monarch
+            Initiative and Open Biological and Biomedical Ontologies (OBO).
           </p>
           <ul>
             <li>
-              The OBO Academy develops Self-paced tutorials in the OBOOK, hosts fortnightly live training sessions on Zoom, and maintains a YouTube channel with lesson recordings.
+              The OBO Academy develops Self-paced tutorials in the OBOOK, hosts
+              fortnightly live training sessions on Zoom, and maintains a
+              YouTube channel with lesson recordings.
             </li>
-            <li>Youtube channel: <a href="https://www.youtube.com/@obo-academy" target="_blank">https://www.youtube.com/@obo-academy</a></li>
-            <li>Join the OBO Academy mailing list using <a href="https://groups.google.com/g/obo-academy" target="_blank">https://groups.google.com/g/obo-academy</a></li>
+            <li>
+              Youtube channel:
+              <a href="https://www.youtube.com/@obo-academy" target="_blank"
+                >https://www.youtube.com/@obo-academy</a
+              >
+            </li>
+            <li>
+              Join the OBO Academy mailing list using
+              <a href="https://groups.google.com/g/obo-academy" target="_blank"
+                >https://groups.google.com/g/obo-academy</a
+              >
+            </li>
           </ul>
         </li>
         <li>
-          <a href="https://mondo.monarchinitiative.org" target="_blank">The Mondo Disease Ontology (Mondo)</a>
+          <a href="https://mondo.monarchinitiative.org" target="_blank"
+            >The Mondo Disease Ontology (Mondo)</a
+          >
           <p>
-            Mondo aims to harmonize disease definitions across the world. The name Mondo comes from the latin word ‘mundus’ and means ‘for the world.’
+            Mondo aims to harmonize disease definitions across the world. The
+            name Mondo comes from the latin word ‘mundus’ and means ‘for the
+            world.’
           </p>
           <ul>
             <li>
-              The Mondo Outreach calls aim to bring together stakeholders and clinical terminology experts to discuss specific use cases and requirements.
-              Our goal is to improve Mondo with a focus on our user needs.
+              The Mondo Outreach calls aim to bring together stakeholders and
+              clinical terminology experts to discuss specific use cases and
+              requirements. Our goal is to improve Mondo with a focus on our
+              user needs.
             </li>
             <li>
-              These calls are scheduled for the second Thursday of every month. You can find more information about the calls <a href="https://mondo.monarchinitiative.org/pages/workshop/#outreach" target="_blank">here</a>.
-            </li> 
-            <li> 
-              You can find the schedule with Zoom information <a href="https://docs.google.com/document/d/1MStpDyh9LOZYJTjLtnpOsNYc3HaeU-bz0CHAI9xOjfQ/edit#heading=h.6sqkx1xhumse" target="_blank">here</a>.
+              These calls are scheduled for the second Thursday of every month.
+              You can find more information about the calls
+              <a
+                href="https://mondo.monarchinitiative.org/pages/workshop/#outreach"
+                target="_blank"
+                >here</a
+              >.
+            </li>
+            <li>
+              You can find the schedule with Zoom information
+              <a
+                href="https://docs.google.com/document/d/1MStpDyh9LOZYJTjLtnpOsNYc3HaeU-bz0CHAI9xOjfQ/edit#heading=h.6sqkx1xhumse"
+                target="_blank"
+                >here</a
+              >.
             </li>
           </ul>
         </li>
         <li>
-          <a href="https://linkml.io/linkml/" target="_blank">LinkML (the Linked data Modeling Language)</a>
+          <a href="https://linkml.io/linkml/" target="_blank"
+            >LinkML (the Linked data Modeling Language)</a
+          >
           <p>
-            LinkML is a flexible modeling language that allows you to author schemas in YAML that describe the structure of your data.
+            LinkML is a flexible modeling language that allows you to author
+            schemas in YAML that describe the structure of your data.
           </p>
           <p>
-            LinkML is used to model the Monarch data model, and we welcome your contributions to the LinkML community.
-            You can learn more <a href="https://linkml.io/linkml/faq/contributing.html" target="_blank">here</a>.
+            LinkML is used to model the Monarch data model, and we welcome your
+            contributions to the LinkML community. You can learn more
+            <a
+              href="https://linkml.io/linkml/faq/contributing.html"
+              target="_blank"
+              >here</a
+            >.
           </p>
         </li>
       </ul>
@@ -58,8 +102,12 @@
     <AppSection>
       <AppHeading>Follow us to learn more!</AppHeading>
       <p>
-        Stay updated on the latest from the Monarch Initiative by joining our 
-        <a href="https://groups.google.com/a/tislab.org/g/monarch-users" target="_blank">user mailing list</a>.
+        Stay updated on the latest from the Monarch Initiative by joining our
+        <a
+          href="https://groups.google.com/a/tislab.org/g/monarch-users"
+          target="_blank"
+          >user mailing list</a
+        >.
       </p>
       <AppFlex direction="col" align-h="center">
         <!-- <AppTile
@@ -77,10 +125,7 @@
           text="Monarch LinkedIn Profile"
           to="https://www.linkedin.com/company/the-monarch-initiative/"
         />
-        <AppButton
-          text="Monarch Blog"
-          to="https://monarchinit.medium.com/"
-        />
+        <AppButton text="Monarch Blog" to="https://monarchinit.medium.com/" />
         <AppButton
           text="Monarch YouTube Channel"
           to="https://www.youtube.com/@monarchinitiative"
@@ -90,8 +135,12 @@
     <AppSection>
       <AppHeading>For questions or technical support</AppHeading>
       <p>
-        We welcome all questions, requests, and feedback! Please submit your inquiry to our <a href="https://github.com/monarch-initiative/helpdesk" target="_blank">Monarch Helpdesk</a>, and we will connect you with someone who can help.
+        We welcome all questions, requests, and feedback! Please submit your
+        inquiry to our
+        <a href="https://github.com/monarch-initiative/helpdesk" target="_blank"
+          >Monarch Helpdesk</a
+        >, and we will connect you with someone who can help.
       </p>
-    </AppSection>      
+    </AppSection>
   </AppSection>
 </template>
