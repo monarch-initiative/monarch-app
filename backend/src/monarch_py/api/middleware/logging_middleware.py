8bba242b3ac4e6fd5a0cabc4e8f0a1ce1bcfd345
Kevin Schaper <kevinschaper@gmail.com>
2023-10-18 13:37:37 -0700

    Request logging (#414)
    Hooks request logging up to loguru via fastapi middleware
    Also instantiates CurieService() on startup to get rid of the stall that
    happens to the first request a worker sees, reduce stdout output and
    share the one singleton across workers (I think)

---
+++ backend/src/monarch_py/api/middleware/logging_middleware.py
@@ -0,0 +1,40 @@
+"""Middleware to log requests."""
+from loguru import logger
+import logging
+
+from fastapi import Request
+from fastapi.logger import logger as fastapi_logger
+from starlette.middleware.base import BaseHTTPMiddleware
+
+fastapi_logger.setLevel(logging.INFO)
+
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+
+    """Middleware to log requests."""
+
+    async def dispatch(self, request: Request, call_next):
+        """
+        Log requests.
+
+        :param request: The request.
+        :param call_next: The next call.
+        :param logger: The logger.
+        :return: The response.
+        """
+        # Log request method and URL
+        logger.info(f"Request URL: {request.url} | Method: {request.method}")
+
+        # Log request headers
+        # logger.info(f"Headers: {dict(request.headers)}")
+
+        # If you need the request body, handle with care:
+        # body = await request.body()
+        # logger.info(f"Body: {body.decode()}")
+        #
+        # Since the body is read and can't be read again,
+        # you need to make it available for the actual route again
+        # request._body = body
+
+        response = await call_next(request)
+        return response
