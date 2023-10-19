08a2f3a5c155e9986ce642e70e1a79d6ed029917
Kevin Schaper <kevinschaper@gmail.com>
2023-10-19 09:57:52 -0700

    formatting

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,9 +13,10 @@
       This web app is the
       <strong v-if="apiName === 'beta'">BETA VERSION</strong
       ><strong v-if="apiName === 'dev'">DEV VERSION</strong> successor to the
-      <AppLink to="https://previous.monarchinitiative.org/">old web app here</AppLink>.
-      Not all features are implemented yet. Please use the feedback form to tell
-      us what you think!
+      <AppLink to="https://previous.monarchinitiative.org/"
+        >old web app here</AppLink
+      >. Not all features are implemented yet. Please use the feedback form to
+      tell us what you think!
     </TheBanner>
 
     <TheHeader />
