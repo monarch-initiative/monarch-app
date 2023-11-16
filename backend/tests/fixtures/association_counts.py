cc594226a0bcb80d2cab628906bea949ea6fae2d
Kevin Schaper <kevinschaper@gmail.com>
2023-11-16 10:40:31 -0800

    Update model & fixtures, adds down ranking for deprecated entities

--- backend/tests/fixtures/association_counts.py
+++ backend/tests/fixtures/association_counts.py
@@ -3,10 +3,4 @@ import pytest
 
 @pytest.fixture
 def association_counts():
-    return {
-        "items": [
-            {"label": "Phenotypes", "count": 4027, "category": "biolink:DiseaseToPhenotypicFeatureAssociation"},
-            {"label": "Causal Genes", "count": 124, "category": "biolink:CausalGeneToDiseaseAssociation"},
-            {"label": "Correlated Genes", "count": 151, "category": "biolink:CorrelatedGeneToDiseaseAssociation"},
-        ]
-    }
+    return {"items": []}
--- backend/tests/fixtures/autocomplete_response.py
+++ backend/tests/fixtures/autocomplete_response.py
@@ -27,6 +27,7 @@ def autocomplete_response():
                     "id": "MONDO:0001083",
                     "category": "biolink:Disease",
                     "name": "Fanconi renotubular syndrome",
+                    "description": "A genetic or acquired disorder characterized by impairment of the function of the proximal tubules of the kidney. It results in decreased reabsorption of electrolytes, glucose, amino acids, and other nutrients.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "De toni-Fanconi syndrome",
@@ -42,12 +43,12 @@ def autocomplete_response():
                         "infantile nephropathic cystinosis",
                         "toni-debre-Fanconi syndrome",
                     ],
-                    "description": "A genetic or acquired disorder characterized by impairment of the function of the proximal tubules of the kidney. It results in decreased reabsorption of electrolytes, glucose, amino acids, and other nutrients.",
                 },
                 {
                     "id": "MONDO:0009215",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group A",
+                    "description": "Fanconi anemia caused by mutations of the FANCA gene. FANCA gene mutations are the most common cause of Fanconi anemia. This gene provides instructions for making a protein that is involved in the Fanconi anemia (FA) pathway.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "Estren-Dameshek variant of Fanconi Anemia",
@@ -62,12 +63,12 @@ def autocomplete_response():
                         "Fanconi anemia complementation group type A",
                         "Fanconi anemia, complementation group A",
                     ],
-                    "description": "Fanconi anemia caused by mutations of the FANCA gene. FANCA gene mutations are the most common cause of Fanconi anemia. This gene provides instructions for making a protein that is involved in the Fanconi anemia (FA) pathway.",
                 },
                 {
                     "id": "MONDO:0013566",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group L",
+                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the FANCL gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCL",
@@ -79,12 +80,12 @@ def autocomplete_response():
                         "Fanconi anemia complementation group type L",
                         "Fanconi anemia, complementation group L",
                     ],
-                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the FANCL gene.",
                 },
                 {
                     "id": "MONDO:0010953",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group E",
+                    "description": "Fanconi anemia caused by mutations of the FANCE gene. This is a protein coding gene. It is required for the nuclear accumulation of FANCC and provides a critical bridge between the FA complex and FANCD2.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCE",
@@ -96,7 +97,6 @@ def autocomplete_response():
                         "Fanconi anemia, complementation group E",
                         "face",
                     ],
-                    "description": "Fanconi anemia caused by mutations of the FANCE gene. This is a protein coding gene. It is required for the nuclear accumulation of FANCC and provides a critical bridge between the FA complex and FANCD2.",
                 },
                 {
                     "id": "MONDO:0024525",
@@ -120,6 +120,7 @@ def autocomplete_response():
                     "id": "MONDO:0019391",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia",
+                    "description": "Fanconi anemia (FA) is a hereditary DNA repair disorder characterized by progressive pancytopenia with bone marrow failure, variable congenital malformations and predisposition to develop hematological or solid tumors.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "Fanconi anemia",
@@ -130,12 +131,12 @@ def autocomplete_response():
                         "pancytopenia, congenital",
                         "primary erythroid hypoplasia",
                     ],
-                    "description": "Fanconi anemia (FA) is a hereditary DNA repair disorder characterized by progressive pancytopenia with bone marrow failure, variable congenital malformations and predisposition to develop hematological or solid tumors.",
                 },
                 {
                     "id": "MONDO:0013248",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group O",
+                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the RAD51C gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCO",
@@ -147,12 +148,12 @@ def autocomplete_response():
                         "RAD51C Fanconi anemia",
                         "Rad51C Fanconi anemia",
                     ],
-                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the RAD51C gene.",
                 },
                 {
                     "id": "MONDO:0013499",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group P",
+                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the SLX4 gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCP",
@@ -164,12 +165,12 @@ def autocomplete_response():
                         "SLX4 Fanconi anemia",
                         "Slx4 Fanconi anemia",
                     ],
-                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the SLX4 gene.",
                 },
                 {
                     "id": "MONDO:0014985",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group V",
+                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the MAD2L2 gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCV",
@@ -181,12 +182,12 @@ def autocomplete_response():
                         "Fanconi anemia, complementation GROUP V",
                         "MAD2L2 Fanconi anemia",
                     ],
-                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the MAD2L2 gene.",
                 },
                 {
                     "id": "MONDO:0010351",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group B",
+                    "description": "Fanconi anemia caused by mutations of the FANCB gene. This gene encodes the protein for complementation group B.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FA2",
@@ -200,12 +201,12 @@ def autocomplete_response():
                         "Fanconi pancytopenia type 2",
                         "Fanconi pancytopenia, type 2",
                     ],
-                    "description": "Fanconi anemia caused by mutations of the FANCB gene. This gene encodes the protein for complementation group B.",
                 },
                 {
                     "id": "MONDO:0012565",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group N",
+                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the PALB2 gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCN",
@@ -216,12 +217,12 @@ def autocomplete_response():
                         "Fanconi anemia, complementation group N",
                         "PALB2 Fanconi anemia",
                     ],
-                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the PALB2 gene.",
                 },
                 {
                     "id": "MONDO:0014986",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group R",
+                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the RAD51 gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCR",
@@ -232,12 +233,12 @@ def autocomplete_response():
                         "Fanconi anemia, complementation GROUP R",
                         "RAD51 Fanconi anemia",
                     ],
-                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the RAD51 gene.",
                 },
                 {
                     "id": "MONDO:0014987",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group U",
+                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the XRCC2 gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCU",
@@ -248,20 +249,20 @@ def autocomplete_response():
                         "Fanconi anemia, complementation GROUP U",
                         "XRCC2 Fanconi anemia",
                     ],
-                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the XRCC2 gene.",
                 },
                 {
                     "id": "MONDO:0100136",
                     "category": "biolink:Disease",
                     "name": "obsolete Fanconia anemia complementation group M",
+                    "description": "OBSOLETE Any Fanconi anemia in which the cause of the disease is a mutation in the FANCM gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": ["FANCM Fanconi anemia", "Fanconi anemia caused by mutation in FANCM"],
-                    "description": "OBSOLETE Any Fanconi anemia in which the cause of the disease is a mutation in the FANCM gene.",
                 },
                 {
                     "id": "MONDO:0009213",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group C",
+                    "description": "Fanconi anemia caused by mutations of the FANCC gene. This gene provides instructions for making a protein that delays the onset of apoptosis and promotes homologous recombination repair of damaged DNA.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FA3",
@@ -275,12 +276,12 @@ def autocomplete_response():
                         "Fanconi pancytopenia, type 3",
                         "facc",
                     ],
-                    "description": "Fanconi anemia caused by mutations of the FANCC gene. This gene provides instructions for making a protein that delays the onset of apoptosis and promotes homologous recombination repair of damaged DNA.",
                 },
                 {
                     "id": "MONDO:0009214",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group D2",
+                    "description": "Fanconi anemia caused by mutations of the FANCD2 gene. This gene is involved in the repair of DNA double-strand breaks, both by homologous recombination and single-strand annealing.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FA4",
@@ -293,12 +294,12 @@ def autocomplete_response():
                         "Fanconi pancytopenia type 4",
                         "Fanconi pancytopenia, type 4",
                     ],
-                    "description": "Fanconi anemia caused by mutations of the FANCD2 gene. This gene is involved in the repair of DNA double-strand breaks, both by homologous recombination and single-strand annealing.",
                 },
                 {
                     "id": "MONDO:0012186",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group I",
+                    "description": "Fanconi anemia caused by mutations in the FANCI gene, encoding Fanconi anemia group I protein.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCI",
@@ -308,12 +309,12 @@ def autocomplete_response():
                         "Fanconi anemia complementation group type I",
                         "Fanconi anemia, complementation group I",
                     ],
-                    "description": "Fanconi anemia caused by mutations in the FANCI gene, encoding Fanconi anemia group I protein.",
                 },
                 {
                     "id": "MONDO:0014108",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group Q",
+                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the ERCC4 gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "ERCC4 Fanconi anemia",
@@ -323,12 +324,12 @@ def autocomplete_response():
                         "Fanconi anemia complementation group type Q",
                         "Fanconi anemia, complementation group Q",
                     ],
-                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the ERCC4 gene.",
                 },
                 {
                     "id": "MONDO:0014638",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group T",
+                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the UBE2T gene.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCT",
@@ -338,12 +339,12 @@ def autocomplete_response():
                         "Fanconi anemia, complementation group T",
                         "UBE2T Fanconi anemia",
                     ],
-                    "description": "Any Fanconi anemia in which the cause of the disease is a mutation in the UBE2T gene.",
                 },
                 {
                     "id": "MONDO:0011325",
                     "category": "biolink:Disease",
                     "name": "Fanconi anemia complementation group F",
+                    "description": "Fanconi anemia caused by mutations of the FANCF gene. This gene encodes a polypeptide with homology to the prokaryotic RNA-binding protein ROM.",
                     "provided_by": "phenio_nodes",
                     "synonym": [
                         "FANCF",
@@ -352,7 +353,6 @@ def autocomplete_response():
                         "Fanconi anemia complementation group type F",
                         "Fanconi anemia, complementation group F",
                     ],
-                    "description": "Fanconi anemia caused by mutations of the FANCF gene. This gene encodes a polypeptide with homology to the prokaryotic RNA-binding protein ROM.",
                 },
             ],
         },
