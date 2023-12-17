e16bb0864ba728f651cb854e017b5372ed589533
Glass <glass.ships@outlook.com>
2023-12-16 21:34:42 -0700

    Update entity_utils.py
    Add `get_uri` entity util back in for tests to pass

--- backend/src/monarch_py/utils/entity_utils.py
+++ backend/src/monarch_py/utils/entity_utils.py
@@ -6,6 +6,11 @@ __all__ = [
 ]
 
 
+def get_uri(id: str) -> str:
+    """Returns the URI for the given CURIE."""
+    return converter.expand(id)
+
+
 def get_expanded_curie(id: str) -> ExpandedCurie:
     """Returns the URI for the given CURIE."""
     return ExpandedCurie(id=id, url=converter.expand(id))
