b8b675b2651512331b03cc889d4d8a53ec5ad19f
Kevin Schaper <kevinschaper@gmail.com>
2024-01-03 14:50:04 -0800

    Removed extra failing test for semsimian search by get, parameterized the one that works to test split+trim code

--- backend/tests/api/test_semsim_router.py
+++ backend/tests/api/test_semsim_router.py
@@ -9,31 +9,15 @@ from monarch_py.api.semsim import router
 client = TestClient(router)
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_semsim(semsim):
-    ...
-
-
-@patch("monarch_py.api.semsim._search")
-def test_get_search(mock_search):
-    mock_search.return_value = ["Term1", "Term2"]
-
-    termset = "HP:123, HP:456"
-    group = "Human Genes"
-    limit = 5
-
-    response = client.get(f"/search/{termset}/{group}?limit={limit}")
-
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == ["Term1", "Term2"]
-    mock_search.assert_called_once_with(termset=["HP:123", "HP:456"], prefix="Human Genes", limit=5)
-
-
 @patch("monarch_py.api.config.SemsimianHTTPRequester.search")
-def test_search(mock_search):
+@pytest.mark.parametrize("termset", [
+    "HP:123,HP:456",
+    "HP:123, HP:456",
+    " HP:123, HP:456 "
+])
+def test_get_search(mock_search, termset: str):
     mock_search.return_value = MagicMock()
 
-    termset = "HP:123, HP:456"
     group = SemsimSearchGroup.HGNC
     limit = 5
 
@@ -41,3 +25,4 @@ def test_search(mock_search):
 
     assert response.status_code == status.HTTP_200_OK
     mock_search.assert_called_once_with(termset=["HP:123", "HP:456"], prefix=group.name, limit=limit)
+
