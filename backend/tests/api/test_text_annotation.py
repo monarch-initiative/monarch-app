11fa8a8a15e0af558e02ede2a37a363a3f011b1e
Kevin Schaper <kevinschaper@gmail.com>
2024-01-03 16:15:08 -0800

    Add api tests for text annotator

---
+++ backend/tests/api/test_text_annotation.py
@@ -0,0 +1,23 @@
+import pytest
+from fastapi.testclient import TestClient
+from fastapi import status
+from unittest.mock import MagicMock, patch
+from monarch_py.api.text_annotation import router
+
+client = TestClient(router)
+
+
+@patch("monarch_py.implementations.spacy.spacy_implementation.SpacyImplementation.annotate_text")
+def test_get_annotate(mock_annotate):
+    mock_annotate.return_value = [MagicMock()]
+    response = client.get("/annotate/Ehlers-Danlos syndrome")
+    assert response.status_code == 200
+    mock_annotate.assert_called_with("Ehlers-Danlos syndrome")
+
+
+@patch("monarch_py.implementations.spacy.spacy_implementation.SpacyImplementation.annotate_text")
+def test_post_annotation(mock_annotate):
+    mock_annotate.return_value = [MagicMock()]
+    response = client.post("/annotate", json={"content": "Ehlers-Danlos syndrome"})
+    assert response.status_code == 200
+    mock_annotate.assert_called_with("Ehlers-Danlos syndrome")
