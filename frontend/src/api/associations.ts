295227e1ecbb1a78fad2ee713a216f1b931f8011
glass-ships <glass.ships@outlook.com>
2024-01-12 15:25:28 -0700

    add download flag for assoc table

--- frontend/src/api/associations.ts
+++ frontend/src/api/associations.ts
@@ -26,7 +26,6 @@ export const getAssociations = async (
   /** make query */
   const url = `${apiUrl}/entity/${nodeId}/${associationCategory}`;
   const response = await request<AssociationTableResults>(url, params);
-
   return response;
 };
 
