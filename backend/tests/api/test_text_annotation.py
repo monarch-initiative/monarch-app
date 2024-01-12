e7915e8008affd34d8674f56fc071121cca66fb4
Kevin Schaper <kevinschaper@gmail.com>
2024-01-11 16:01:26 -0800

    Don't put MagicMock in a list! MagicMock already knows how to be a list!

--- backend/tests/api/test_text_annotation.py
+++ backend/tests/api/test_text_annotation.py
@@ -1,13 +1,14 @@
 from fastapi.testclient import TestClient
 from unittest.mock import MagicMock, patch
 from monarch_py.api.text_annotation import router
+from monarch_py.datamodels.model import TextAnnotationResult
 
 client = TestClient(router)
 
 
 @patch("monarch_py.implementations.spacy.spacy_implementation.SpacyImplementation.annotate_text")
 def test_get_annotate(mock_annotate):
-    mock_annotate.return_value = [MagicMock()]
+    mock_annotate.return_value = MagicMock()
     response = client.get("/annotate/Ehlers-Danlos syndrome")
     assert response.status_code == 200
     mock_annotate.assert_called_with("Ehlers-Danlos syndrome")
@@ -15,7 +16,7 @@ def test_get_annotate(mock_annotate):
 
 @patch("monarch_py.implementations.spacy.spacy_implementation.SpacyImplementation.annotate_text")
 def test_post_annotation(mock_annotate):
-    mock_annotate.return_value = [MagicMock()]
+    mock_annotate.return_value = MagicMock()
     response = client.post("/annotate", json={"content": "Ehlers-Danlos syndrome"})
     assert response.status_code == 200
     mock_annotate.assert_called_with("Ehlers-Danlos syndrome")
