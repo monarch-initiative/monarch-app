8aa2932d1f6d9f8cf57ce7818e61b8cfcdc7deed
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-01-12 18:24:27 -0500

    fix lint

--- frontend/src/api/associations.ts
+++ frontend/src/api/associations.ts
@@ -24,7 +24,7 @@ export const getAssociations = async (
   /** make query */
   const url = `${apiUrl}/entity/${nodeId}/${associationCategory}`;
   const response = await request<AssociationTableResults>(url, params);
-  
+
   return response;
 };
 
