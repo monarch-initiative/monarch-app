f38da68c5bc4d97895a71d81ed85d1e959bb5c77
glass-ships <glass.ships@outlook.com>
2024-01-29 14:51:49 -0700

    fix text annotator

--- backend/src/monarch_py/api/text_annotation.py
+++ backend/src/monarch_py/api/text_annotation.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Path, Response
+from fastapi import APIRouter, Query, Response
 from typing import List
 
 from monarch_py.api.additional_models import TextAnnotationRequest
@@ -8,9 +8,9 @@ from monarch_py.datamodels.model import TextAnnotationResult
 router = APIRouter(tags=["text_annotation"], responses={404: {"description": "Not Found"}})
 
 
-@router.get("/annotate/{content}")
-def _annotate(content: str = Path(title="The text content to annotate")) -> str:
-    return Response(content=spacyner().annotate_text(content), media_type="text/html")
+@router.get("/annotate")
+def _annotate(text: str = Query(default="", title="The text content to annotate")) -> str:
+    return Response(content=spacyner().annotate_text(text), media_type="text/html")
 
 
 @router.post("/annotate")
@@ -18,11 +18,11 @@ def _post_annotate(request: TextAnnotationRequest) -> str:
     return _annotate(request.content)
 
 
-@router.get("/annotate/entities?text={content}")
-def _entities(text: str) -> List[TextAnnotationResult]:
+@router.get("/annotate/entities")
+def _entities(text: str = Query(default="", title="")) -> List[TextAnnotationResult]:
     return spacyner().get_annotated_entities(text)
 
 
 @router.post("/annotate/entities")
 def _post_entities(request: TextAnnotationRequest) -> List[TextAnnotationResult]:
-    return _entities(request)
+    return _entities(request.content)
