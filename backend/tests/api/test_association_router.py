dfe720c68a60a620bae9e13a54681c2df19acb74
Glass <glass.ships@outlook.com>
2024-01-29 14:56:09 -0700

    add subj/obj taxon, category, namespace to assoc api endpoint (#548)
    Closes #546 
    
    - Adds some extra parameters to associations endpoint and
    SolrImplementation.get_associations
    - Adds api tests for proper parameters being passed to SI
    - Small fix to TextAnnotator
    - Update to SI's Query model methods for adding facet fields
    
    ---------
    
    Co-authored-by: Kevin Schaper <kevinschaper@gmail.com>

--- backend/tests/api/test_association_router.py
+++ backend/tests/api/test_association_router.py
@@ -1,7 +1,9 @@
+import urllib
 from unittest.mock import MagicMock, patch
 
 from fastapi.testclient import TestClient
 from httpx import Response
+
 from monarch_py.api.association import router
 
 client = TestClient(router)
@@ -17,3 +19,29 @@ def test_associations(associations):
     assert response.status_code == 200
     assert response.headers["content-type"] == "application/json"
     assert response.json() == associations
+
+
+@patch("monarch_py.implementations.solr.solr_implementation.SolrImplementation.get_associations")
+def test_associations_params(mock_get_assoc):
+    """Test that the correct parameters are passed to the monarch api"""
+    mock_get_assoc.return_value = MagicMock()
+
+    params = {
+        "category": ["biolink:Association", "biolink:GeneToPhenotypicFeatureAssociation"],
+        "subject": ["HGNC:0000001", "HGNC:0000002"],
+        "predicate": ["biolink:interacts_with"],
+        "object": ["MONDO:0000001", "MONDO:0000002"],
+        "entity": ["MONDO:0000002", "HGNC:0000001"],
+        "subject_category": ["biolink:Disease", "biolink:Gene"],
+        "subject_namespace": ["HGNC", "ZFIN"],
+        "subject_taxon": ["NCBITaxon:1234", "NCBITaxon:1"],
+        "object_taxon": ["NCBITaxon:9876", "NCBITaxon:2"],
+        "object_category": ["biolink:Gene", "biolink:PhenotypicFeature"],
+        "object_namespace": ["MONDO", "HP"],
+        "direct": True,
+        "offset": 0,
+        "limit": 20,
+    }
+    query_string = urllib.parse.urlencode(params, doseq=True)
+    client.get(f"/all?{query_string}")
+    mock_get_assoc.assert_called_with(**params)
--- backend/tests/api/test_histopheno_router.py
+++ backend/tests/api/test_histopheno_router.py
@@ -2,7 +2,9 @@ from unittest.mock import MagicMock, patch
 
 from fastapi.testclient import TestClient
 from httpx import Response
+
 from monarch_py.api.histopheno import router
+from monarch_py.datamodels.model import HistoPheno
 
 client = TestClient(router)
 
@@ -17,3 +19,10 @@ def test_histopheno(histopheno):
     assert response.status_code == 200
     assert response.headers["content-type"] == "application/json"
     assert response.json() == histopheno
+
+
+@patch("monarch_py.implementations.solr.solr_implementation.SolrImplementation.get_histopheno")
+def test_histopheno_params(mock_get_histopheno, histopheno):
+    mock_get_histopheno.return_value = HistoPheno(**histopheno)
+    client.get("/HP:0000001")
+    mock_get_histopheno.assert_called_with("HP:0000001")
