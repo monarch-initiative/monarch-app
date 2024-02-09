f852625768a6bea6ec77a2e67c72573587324bc6
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-02-09 16:10:25 -0500

    add color to category icons (#545)
    - include tailwind color palette (professionally curated). manually pick
    from that, and assign a color to each category type.
    - remove association icons
    - remove hard-coded circle in category icons
    - refactor icon component and simplify. add outline circle dynamically.
    make solid fill and white stroke inside for categories. tweak css.
    - add slot to app tile component
    - rework homepage to use two node icons for association tiles

--- frontend/src/components/AppIcon.vue
+++ frontend/src/components/AppIcon.vue
@@ -6,25 +6,23 @@
   <component
     :is="customIcon"
     v-if="isCustom"
-    class="custom"
-    :style="{ '--thickness': thickness }"
-    :data-icon="icon"
+    :class="['app-icon', type]"
     aria-hidden="true"
+    @vue:updated="({ el }: VNode) => customMounted(el, true)"
   />
   <FontAwesomeIcon
     v-else-if="fontAwesome"
     :icon="fontAwesome"
-    class="fa"
-    :style="{ '--thickness': thickness }"
+    class="app-icon"
     aria-hidden="true"
   />
   <svg
     v-else-if="initials"
-    viewBox="-10 -10 120 120"
-    class="initials"
-    :style="{ '--thickness': thickness }"
+    viewBox="0 0 100 100"
+    class="app-icon initials"
+    @vue:mounted="({ el }: VNode) => customMounted(el)"
   >
-    <circle fill="none" stroke="currentColor" cx="50" cy="50" r="55" />
+    <circle class="outline" cx="50" cy="50" r="50" />
     <text x="50" y="54">
       {{ initials }}
     </text>
@@ -32,12 +30,12 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineAsyncComponent, ref } from "vue";
+import { computed, defineAsyncComponent, ref, type VNode } from "vue";
 import { kebabCase } from "lodash";
 import type { IconName, IconPrefix } from "@fortawesome/fontawesome-svg-core";
 import { findIconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { useElementSize } from "@vueuse/core";
+import { getCategoryColor } from "@/api/categories";
 
 type Props = {
   /**
@@ -73,58 +71,82 @@ const customIcon = defineAsyncComponent(async () => {
   }
 });
 
+/** special icon type based on name prefix */
+const type = computed(() => {
+  if (props.icon.startsWith("category-")) return "category";
+  if (props.icon.startsWith("resource-")) return "resource";
+  return "";
+});
+
 /** initials */
 const initials = computed(
   () =>
     props.icon
-      ?.replace(/^category-/, "")
+      ?.replace(new RegExp(`^${type.value}-`), "")
       .split("-")
       .map((word) => (word[0] || "").toUpperCase())
       .slice(0, 2)
       .join("") || "",
 );
 
-/** dynamic line thickness */
-const thickness = computed(() => {
-  /** leave opportunity for thickness based on absolute size in the future */
-  return 5;
-});
+/** when custom icon mounted */
+function customMounted(element: VNode["el"], createCircle = false) {
+  /** add child elements to category icon */
+  if (
+    element &&
+    element instanceof SVGSVGElement &&
+    type.value === "category"
+  ) {
+    /** set color for category */
+    element.style.setProperty("--color", getCategoryColor(props.icon));
+
+    if (createCircle) {
+      /** create circle outline */
+      const outline = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "circle",
+      );
+      outline.setAttribute("cx", "50");
+      outline.setAttribute("cy", "50");
+      outline.setAttribute("r", "50");
+      outline.classList.add("outline");
+      element.insertBefore(outline, element.firstChild!);
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.custom {
+.app-icon {
   height: 1em;
+}
 
-  /** resource icon styles */
-  &[data-icon^="resource-"] {
-    filter: brightness(0.2);
-  }
+.category {
+  fill: none;
+  stroke: $white;
+  stroke-width: 5;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
 
-  /** category icon styles */
-  &[data-icon^="category-"],
-  &[data-icon^="association-"] {
-    fill: none;
-    stroke: currentColor;
-    stroke-width: var(--thickness);
-    stroke-linecap: round;
-    stroke-linejoin: round;
-  }
+:deep(.outline) {
+  stroke: none;
+  fill: var(--color, currentColor);
+  transform: scale(1.1);
+  transform-box: fill-box;
+  transform-origin: center;
 }
 
 .initials {
-  height: 1em;
-
-  circle {
-    fill: none;
-    stroke: currentColor;
-    stroke-width: var(--thickness);
-  }
-
   text {
-    fill: currentColor;
+    fill: $white;
     text-anchor: middle;
     dominant-baseline: middle;
     font-size: 50px;
   }
+}
+
+.resource {
+  filter: brightness(0.2);
 }
 </style>
