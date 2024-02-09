be573c1e24d68b287989abf7cb0d382d27e29fa0
Corey Cox <69321580+amc-corey-cox@users.noreply.github.com>
2024-02-09 15:43:55 -0600

    Add page and placeholders for how-to development.

---
+++ frontend/src/pages/about/PageHowTo.vue
@@ -0,0 +1,40 @@
+<!-- 
+  how to
+ -->
+
+<template>
+  <AppSection>
+    <AppHeading>Getting Started with Monarch KG</AppHeading>
+    <p>
+      This is a placeholder for the how-to page. It will contain information on how to use the Monarch KG through the website.
+    </p>
+  </AppSection>
+
+  <AppSection>
+    <AppHeading>How to explore the Monarch KG using Monarch Web</AppHeading>
+  </AppSection>
+
+  <AppSection design="fill">
+    <AppHeading>Goals</AppHeading>
+  </AppSection>
+
+  <AppSection>
+    <AppHeading>
+      <AppIcon icon="resource-upheno" />
+      Heading
+    </AppHeading>
+
+    <p> Info
+      <AppLink
+        to="https://github.com/obophenotype/upheno/wiki/Phenotype-Ontologies-Reconciliation-Effort"
+        >Phenotype Ontologies Reconciliation Effort</AppLink
+      >.
+    </p>
+    <AppButton
+      to="https://obophenotype.github.io/upheno/"
+      text="Learn More"
+      icon="arrow-right"
+    />
+  </AppSection>
+
+</template>
