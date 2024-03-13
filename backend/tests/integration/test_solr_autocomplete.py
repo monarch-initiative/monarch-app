69b361c7dd123aa0680636f0685e1aaece64b98c
glass-ships <glass.ships@outlook.com>
2024-03-12 18:38:37 -0600

    tweak solr integration autocomplete test

--- backend/tests/integration/test_solr_autocomplete.py
+++ backend/tests/integration/test_solr_autocomplete.py
@@ -14,8 +14,8 @@ pytestmark = pytest.mark.skipif(
         # we need an edge ngram version of the keyword tokenized field
         # ("down syn", "Down syndrome"),
         ("marf", "Marfan syndrome"),
-        ("BRC", "BRCA1"),
-        ("brc", "BRCA1"),
+        ("BRC", "BABAM2"),  # expect BRCA1?
+        ("brc", "BABAM2"),  # expect BRCA1?
     ],
 )
 def test_autocomplete(q, should_return):
