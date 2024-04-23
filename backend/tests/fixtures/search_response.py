49378b75759774490811fcb5e8b2e0832937d92c
Kevin Schaper <kevinschaper@gmail.com>
2024-04-22 18:58:31 -0700

    fixtures and formatting

--- backend/tests/fixtures/search_response.py
+++ backend/tests/fixtures/search_response.py
@@ -5,13 +5,13 @@ import pytest
 def search_response():
     return {
         "responseHeader": {
-            "QTime": 3,
+            "QTime": 2,
             "params": {
                 "mm": "100%",
                 "q": "fanconi",
                 "defType": "edismax",
                 "facet_min_count": "1",
-                "qf": "id^100 name^10 name_t^5 name_ac symbol^10 symbol_t^5 symbol_ac synonym synonym_t synonym_ac description_t",
+                "qf": "id^100 name^10 name_t^5 name_ac symbol^10 symbol_t^5 symbol_ac synonym synonym_t synonym_ac description_t xref",
                 "start": "0",
                 "q.op": "AND",
                 "boost": 'product(if(termfreq(category,"biolink:PhenotypicFeature"),1.1,1),if(termfreq(category,"biolink:Disease"),1.3,1),if(and(termfreq(in_taxon,"NCBITaxon:9606"),termfreq(category,"biolink:Gene")),1.1,1),if(termfreq(deprecated,"true"),0.1,1))',
