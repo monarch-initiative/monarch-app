162261b70c6081a21622082f135d5948508b9136
Kevin Schaper <kevinschaper@gmail.com>
2024-07-09 10:04:52 -0700

    Patches MGI curie expansions that replace the colon with a slash erroneously

--- backend/src/monarch_py/utils/entity_utils.py
+++ backend/src/monarch_py/utils/entity_utils.py
@@ -1,3 +1,4 @@
+import re
 from monarch_py.datamodels.model import ExpandedCurie
 from monarch_py.service.curie_service import converter
 
@@ -6,9 +7,20 @@ __all__ = [
 ]
 
 
+def expansion_patch(url: str):
+    """Any short term patches that we need while waiting for upstream fixes"""
+
+    # This fixes MGI urls that take the form of
+    # https://identifiers.org/MGI/97486
+    # rather than https://identifiers.org/MGI:97486
+    pattern = r"/MGI/(\d+)$"
+    replacement = r"/MGI:\1"
+    return re.sub(pattern, replacement, url)
+
+
 def get_uri(id: str) -> str:
     """Returns the URI for the given CURIE."""
-    return converter.expand(id)
+    return expansion_patch(converter.expand(id))
 
 
 def get_expanded_curie(id: str) -> ExpandedCurie:
