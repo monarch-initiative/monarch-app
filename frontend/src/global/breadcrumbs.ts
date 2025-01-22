903573c5d3a456f684360cd4c5df2785b54d3103
Patrick Golden <ptgolden@email.unc.com>
2025-01-22 14:20:08 -0500

    Add documentation to breadcrumb node filtering

--- frontend/src/global/breadcrumbs.ts
+++ frontend/src/global/breadcrumbs.ts
@@ -3,15 +3,32 @@ import { pick } from "lodash";
 import type { DirectionalAssociation, Node } from "@/api/model";
 import { parse } from "@/util/object";
 
-const breadcrumbKeys = ["id", "category", "name", "in_taxon_label"] as const;
+/**
+ * This constant represents the minimal set of properties needed to render a
+ * link to a Node object. If more properties must be added to render such a link
+ * in the future, add them to this list.
+ *
+ * This is necessary because some pages render a large number of links to nodes.
+ * Each node then must be serialized and inserted into the DOM by Vue. If the
+ * size of nodes is non-trivial (as in, having lots of related nodes, which is
+ * not uncommon), then serializing more than ~50 nodes would cause noticeable
+ * slowdown in the UI. Rendering 1000 nodes would cause the browser to hang for
+ * about a minute.
+ *
+ * To see the issue that initiated this, see:
+ * https://github.com/monarch-initiative/monarch-app/issues/912
+ */
+const BREADCRUMB_KEYS = ["id", "category", "name", "in_taxon_label"] as const;
 
-export type BreadcrumbNode = Pick<Node, (typeof breadcrumbKeys)[number]>;
+export type BreadcrumbNode = Pick<Node, (typeof BREADCRUMB_KEYS)[number]>;
 
 export type Breadcrumb = {
-  /** node we're coming from */
+  /* node we're coming from, represented as a small subset of keys defined above */
   node: BreadcrumbNode;
+
   /** association between the previous node and the current node */
   association: Partial<DirectionalAssociation>;
+
   /**
    * whether breadcrumb doesn't have its own history entry (e.g. implicitly
    * created sub-class association between current node and association
@@ -20,8 +37,9 @@ export type Breadcrumb = {
   noEntry?: boolean;
 };
 
+/* Filter out properties of a node to only include those necessary to render a breadcrumb */
 export function toBreadcrumbNode(node: Node) {
-  const obj: BreadcrumbNode = pick(node, breadcrumbKeys);
+  const obj: BreadcrumbNode = pick(node, BREADCRUMB_KEYS);
   return obj;
 }
 
