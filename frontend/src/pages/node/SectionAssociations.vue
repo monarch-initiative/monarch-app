9c4ff5c0e6c8b4549731bcd3aca79326f1e05be1
varun_divya <divya_varun@med.unc.edu>
2025-03-25 11:53:40 -0400

    clean up

--- frontend/src/pages/node/SectionAssociations.vue
+++ frontend/src/pages/node/SectionAssociations.vue
@@ -14,7 +14,10 @@
 
       <div class="topRow">
         <AppCheckbox
-          v-if="true"
+          v-if="
+            node.category === 'biolink:Gene' &&
+            category?.id.startsWith('biolink:GeneToPheno')
+          "
           v-model="includeOrthologs"
           v-tooltip="
             'Include phenotypes for orthologous genes in the associations table'
