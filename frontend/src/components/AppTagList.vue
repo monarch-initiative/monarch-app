91467efec57e8efe2b2578162db45eaad6647a31
varun_divya <divya_varun@med.unc.edu>
2025-04-24 15:16:51 -0400

    changed tag color

--- frontend/src/components/AppTagList.vue
+++ frontend/src/components/AppTagList.vue
@@ -23,11 +23,11 @@ defineProps<{
 .synonym-tag {
   display: inline-block;
   padding: 0.35em 0.9em;
-  border: 0.07em solid #b2ebf2;
+
   border-radius: 999em;
-  background-color: #e0f7fa;
+  background-color: $light-gray;
   box-shadow: 0 0.07em 0.14em rgba(0, 0, 0, 0.04);
-  color: #006064;
+
   font-weight: 500;
   font-size: 0.85em;
   transition: background-color 0.2s ease;
