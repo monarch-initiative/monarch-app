b66459d430257c1209db1f133bace15396ff7127
varun_divya <divya_varun@med.unc.edu>
2025-04-30 10:41:29 -0400

    clean up

--- frontend/src/components/AppTagList.vue
+++ frontend/src/components/AppTagList.vue
@@ -23,11 +23,9 @@ defineProps<{
 .synonym-tag {
   display: inline-block;
   padding: 0.35em 0.9em;
-
   border-radius: 999em;
   background-color: $light-gray;
   box-shadow: 0 0.07em 0.14em rgba(0, 0, 0, 0.04);
-
   font-weight: 500;
   font-size: 0.85em;
   transition: background-color 0.2s ease;
