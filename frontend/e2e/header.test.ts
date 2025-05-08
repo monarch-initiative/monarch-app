9736820054fe8ba818840f6f52eba7c78a0fd88a
varun_divya <divya_varun@med.unc.edu>
2025-05-08 12:20:04 -0400

    fixed header tests, updated svgs for neo4j and phenotype search

--- frontend/e2e/header.test.ts
+++ frontend/e2e/header.test.ts
@@ -11,7 +11,7 @@ test("Header nav bar collapses on small screens", async ({ page }) => {
   await page.evaluate(() => window.dispatchEvent(new Event("resize")));
   /** get elements of interest */
   const toggle = page.locator("header button").first();
-  const nav = page.locator("nav a").first();
+  const nav = page.locator("nav").first();
 
   /** click toggle button and see if nav hides/shows */
   await expect(toggle).toBeVisible();
