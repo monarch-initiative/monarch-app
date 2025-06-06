b2191b92762aadbd4620d46c757b5a79eb50241f
varun_divya <divya_varun@med.unc.edu>
2025-06-06 15:44:52 -0400

    footer refactor

--- frontend/src/components/TheFooterV2.vue
+++ frontend/src/components/TheFooterV2.vue
@@ -1,10 +1,61 @@
 <template>
   <footer class="footer">
     <div class="container">
-      <div class="logo-column">
-        <ResourceIcon class="footer-logo" />
-        <p>© Monarch Initiative</p>
-        <p>2025. BSD-3</p>
+      <!-- Logo & Social -->
+      <div class="left-column">
+        <div class="logo-column">
+          <ResourceIcon class="footer-logo" />
+          <p>© Monarch Initiative</p>
+          <p>2025. BSD-3</p>
+        </div>
+        <div class="social">
+          <p>Follow us on</p>
+          <div class="icons">
+            <AppLink
+              v-tooltip="'Subscribe'"
+              to="https://groups.google.com/g/monarch-friends/"
+              class="social-icon"
+            >
+              <AppIcon icon="envelope" />
+            </AppLink>
+            <AppLink
+              v-tooltip="'Medium'"
+              to="https://medium.com/@MonarchInit"
+              class="social-icon"
+            >
+              <AppIcon icon="medium" />
+            </AppLink>
+            <AppLink
+              v-tooltip="'GitHub'"
+              to="https://github.com/monarch-initiative"
+              class="social-icon"
+            >
+              <AppIcon icon="github" />
+            </AppLink>
+            <AppLink
+              v-tooltip="'LinkedIn'"
+              to="https://www.linkedin.com/company/the-monarch-initiative"
+              class="social-icon"
+            >
+              <AppIcon icon="linkedin" />
+            </AppLink>
+
+            <AppLink
+              v-tooltip="'YouTube'"
+              to="https://www.youtube.com/@monarchinitiative"
+              class="social-icon"
+            >
+              <AppIcon icon="youtube" />
+            </AppLink>
+            <AppLink
+              v-tooltip="'blusky'"
+              to="https://bsky.app/profile/monarchinitiative.bsky.social"
+              class="social-icon"
+            >
+              <AppIcon icon="social-bluesky" />
+            </AppLink>
+          </div>
+        </div>
       </div>
 
       <div
@@ -27,55 +78,6 @@
             </AppLink>
           </li>
         </ul>
-      </div>
-
-      <div class="social">
-        <p>Follow us on:</p>
-        <div class="icons">
-          <AppLink
-            v-tooltip="'Subscribe'"
-            to="https://groups.google.com/g/monarch-friends/"
-            class="social-icon"
-          >
-            <AppIcon icon="envelope" />
-          </AppLink>
-          <AppLink
-            v-tooltip="'Medium'"
-            to="https://medium.com/@MonarchInit"
-            class="social-icon"
-          >
-            <AppIcon icon="medium" />
-          </AppLink>
-          <AppLink
-            v-tooltip="'GitHub'"
-            to="https://github.com/monarch-initiative"
-            class="social-icon"
-          >
-            <AppIcon icon="github" />
-          </AppLink>
-          <AppLink
-            v-tooltip="'LinkedIn'"
-            to="https://www.linkedin.com/company/the-monarch-initiative"
-            class="social-icon"
-          >
-            <AppIcon icon="linkedin" />
-          </AppLink>
-
-          <AppLink
-            v-tooltip="'YouTube'"
-            to="https://www.youtube.com/@monarchinitiative"
-            class="social-icon"
-          >
-            <AppIcon icon="youtube" />
-          </AppLink>
-          <AppLink
-            v-tooltip="'blusky'"
-            to="https://bsky.app/profile/monarchinitiative.bsky.social"
-            class="social-icon"
-          >
-            <AppIcon icon="social-bluesky" />
-          </AppLink>
-        </div>
       </div>
     </div>
   </footer>
@@ -166,10 +168,11 @@ import navigationMenus from "@/data/navigationMenu.json";
     font-weight: bold;
   }
 }
-.social {
+.left-column {
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 2em;
 }
 
 .social-icon {
@@ -205,5 +208,11 @@ import navigationMenus from "@/data/navigationMenu.json";
   .app-icon[data-icon="bluesky"] {
     color: #1185fe;
   }
+}
+
+.social {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
