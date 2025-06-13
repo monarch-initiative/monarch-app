5f2235cb32a7d2a3e4e0b0a1a21240b066cea724
Glass <glass.ships@outlook.com>
2025-06-12 22:38:45 -0400

    fix curie test

--- backend/tests/unit/test_curie_service.py
+++ backend/tests/unit/test_curie_service.py
@@ -3,32 +3,32 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    ("curie", "expanded_curie_part"),
+    ("curie", "expected"),
     [
         (
             "FB:FBgn0000008",
-            "http://identifiers.org/flybase/FBgn0000008",
+            "identifiers.org/flybase/FBgn0000008",
         ),  # this is a little odd, fb vs. flybase, but both resolve
-        ("FlyBase:FBgn0000008", "http://identifiers.org/fb/FBgn0000008"),
-        ("MONDO:0005737", "http://purl.obolibrary.org/obo/MONDO_0005737"),
-        ("NCBIGene:1017", "https://identifiers.org/ncbigene/1017"),
-        ("ZFIN:ZDB-GENE-980526-166", "https://identifiers.org/zfin/ZDB-GENE-980526-166"),
-        ("MGI:1918910", "https://identifiers.org/MGI/1918910"),
-        ("HP:0000001", "http://purl.obolibrary.org/obo/HP_0000001"),
-        ("GO:0000001", "http://purl.obolibrary.org/obo/GO_0000001"),
-        ("CL:0000001", "http://purl.obolibrary.org/obo/CL_0000001"),
-        ("WB:WBGene00000001", "https://identifiers.org/wb/WBGene00000001"),
-        ("SGD:S000000001", "https://identifiers.org/sgd/S000000001"),
-        ("RGD:1", "https://identifiers.org/rgd/1"),
-        ("HGNC:5", "http://identifiers.org/hgnc/5"),
+        ("FlyBase:FBgn0000008", "identifiers.org/fb/FBgn0000008"),
+        ("MONDO:0005737", "purl.obolibrary.org/obo/MONDO_0005737"),
+        ("NCBIGene:1017", "identifiers.org/ncbigene/1017"),
+        ("ZFIN:ZDB-GENE-980526-166", "identifiers.org/zfin/ZDB-GENE-980526-166"),
+        ("MGI:1918910", "identifiers.org/MGI/1918910"),
+        ("HP:0000001", "purl.obolibrary.org/obo/HP_0000001"),
+        ("GO:0000001", "purl.obolibrary.org/obo/GO_0000001"),
+        ("CL:0000001", "purl.obolibrary.org/obo/CL_0000001"),
+        ("WB:WBGene00000001", "identifiers.org/wb/WBGene00000001"),
+        ("SGD:S000000001", "identifiers.org/sgd/S000000001"),
+        ("RGD:1", "identifiers.org/rgd/1"),
+        ("HGNC:5", "identifiers.org/hgnc/5"),
         ### These prefixes are currently broken in the curie service
-        ("ENSEMBL:ENSG00000157764", "http://identifiers.org/ensembl/ENSG00000157764"),
-        ("OMIM:613647", "http://identifiers.org/mim/613647"),
-        ("UBERON:0000001", "http://purl.obolibrary.org/obo/UBERON_0000001"),
-        ("WormBase:WBGene00000001", "http://identifiers.org/wb/WBGene00000001"),
+        ("ENSEMBL:ENSG00000157764", "identifiers.org/ensembl/ENSG00000157764"),
+        ("OMIM:613647", "identifiers.org/mim/613647"),
+        ("UBERON:0000001", "purl.obolibrary.org/obo/UBERON_0000001"),
+        ("WormBase:WBGene00000001", "identifiers.org/wb/WBGene00000001"),
     ],
 )
-def test_curie_expansion(curie, expanded_curie_part):
+def test_curie_expansion(curie, expected):
     expanded_curie = converter.expand(curie)
-    # print(expanded_curie, expanded_curie_part)
-    assert expanded_curie_part in expanded_curie
+    # print(expanded_curie, expected)
+    assert expanded_curie in expected
