17a1d7bd9a4959c843d8a72d8c6c672c11feba58
varun_divya <divya_varun@med.unc.edu>
2025-06-25 11:20:46 -0400

    moved neo4j and monarch r to kg dropdown

--- frontend/src/data/toolEntityConfig.ts
+++ frontend/src/data/toolEntityConfig.ts
@@ -1,28 +1,26 @@
-export const ENTITY_MAP: Record<
-  string,
-  { id: string; label: string; to?: string; text?: string }
-> = {
-  "Ehlers-Danlos syndrome": {
-    id: "MONDO:0020066",
-    label: "Ehlers-Danlos syndrome",
-    to: "disease-to-phenotype",
-  },
-  "Down syndrome": {
-    id: "MONDO:0008608",
-    label: "Down syndrome",
-    to: "disease-model",
-  },
-  "cystic fibrosis": {
-    id: "MONDO:0009061",
-    label: "Cystic fibrosis",
-    to: "variant-to-disease",
-  },
-  FBN1: {
-    id: "HGNC:3603",
-    label: "FBN1",
-    to: "gene-to-phenotype",
-  },
-};
+export const ENTITY_MAP: Record<string, { id: string; label: string; to?: string; text?: string }> =
+  {
+    'Ehlers-Danlos syndrome': {
+      id: 'MONDO:0020066',
+      label: 'Ehlers-Danlos syndrome',
+      to: 'disease-to-phenotype',
+    },
+    'Down syndrome': {
+      id: 'MONDO:0008608',
+      label: 'Down syndrome',
+      to: 'disease-model',
+    },
+    'cystic fibrosis': {
+      id: 'MONDO:0009061',
+      label: 'Cystic fibrosis',
+      to: 'variant-to-disease',
+    },
+    FBN1: {
+      id: 'HGNC:3603',
+      label: 'FBN1',
+      to: 'gene-to-phenotype',
+    },
+  };
 
 export const KG_TOOL_LINKS: {
   label: string;
@@ -32,32 +30,29 @@ export const KG_TOOL_LINKS: {
   tooltip?: string;
 }[] = [
   {
-    label: "Phenotype Search",
-    icon: "tool-phenotype-search",
-    to: "/search-phenotypes",
-    tooltip: "Start exploring genes and diseases based on phenotype similarity",
+    label: 'Phenotype Search',
+    icon: 'tool-phenotype-search',
+    to: '/search-phenotypes',
+    tooltip: 'Start exploring genes and diseases based on phenotype similarity',
   },
   {
-    label: "Text Annotator",
-    icon: "text-annotator",
-    to: "/text-annotator",
-    tooltip:
-      "Paste or upload text to find references to Monarch Knowledge Graph entities",
+    label: 'Text Annotator',
+    icon: 'text-annotator',
+    to: '/text-annotator',
+    tooltip: 'Paste or upload text to find references to Monarch Knowledge Graph entities',
   },
   {
-    label: "Neo4j",
-    icon: "neoj",
-    to: "https://neo4j.monarchinitiative.org/browser/",
-    tooltip:
-      "Explore the Monarch Knowledge Graph directly in the Neo4j browser",
+    label: 'Neo4j',
+    icon: 'neoj',
+    to: 'https://neo4j.monarchinitiative.org/browser/',
+    tooltip: 'Explore the Monarch Knowledge Graph directly in the Neo4j browser',
     external: true,
   },
   {
-    label: "Monarch R",
-    icon: "monarchr",
-    to: "https://monarch-initiative.github.io/monarchr/articles/monarchr",
-    tooltip:
-      "Query and manipulate Monarch KG data in a tidy-inspired R interface",
+    label: 'Monarch R',
+    icon: 'monarchr',
+    to: 'https://monarch-initiative.github.io/monarchr/articles/monarchr',
+    tooltip: 'Query and manipulate Monarch KG data in a tidy-inspired R interface',
     external: true,
   },
 ];
@@ -70,32 +65,31 @@ export const TOOL_LINKS: {
   tooltip?: string;
 }[] = [
   {
-    label: "Exomiser",
-    icon: "exomiser-logo-banner.png",
-    to: "/tools/exomiser",
-    tooltip: "Explore and prioritize genetic variants",
+    label: 'Exomiser',
+    icon: 'exomiser-logo-banner.png',
+    to: '/tools/exomiser',
+    tooltip: 'Explore and prioritize genetic variants',
     external: true,
   },
   {
-    label: "Phenopackets",
-    icon: "phenopackets-logo.png",
-    to: "/tools/phenopackets",
-    tooltip: "Standard format for sharing phenotypic and clinical data",
+    label: 'Phenopackets',
+    icon: 'phenopackets-logo.png',
+    to: '/tools/phenopackets',
+    tooltip: 'Standard format for sharing phenotypic and clinical data',
     external: true,
   },
   {
-    label: "Mondo",
-    icon: "mondo-logo-banner.png",
-    to: "/ontologies/mondo",
-    tooltip:
-      "Standardized vocabulary for diseases across clinical and research resources",
+    label: 'Mondo',
+    icon: 'mondo-logo-banner.png',
+    to: '/ontologies/mondo',
+    tooltip: 'Standardized vocabulary for diseases across clinical and research resources',
     external: true,
   },
   {
-    label: "Knowledge Graph",
-    icon: "kg-banner",
-    to: "#header",
-    tooltip: "Search and explore genes, diseases, phenotypes, and more",
+    label: 'Knowledge Graph',
+    icon: 'kg-banner',
+    to: '#header',
+    tooltip: 'Search and explore genes, diseases, phenotypes, and more',
     external: true,
   },
 ];
